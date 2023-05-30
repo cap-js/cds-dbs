688c697cf7680c7686201e33daeae2eff56e8154
Patrice Bender <patrice.bender@sap.com>
2023-05-30 11:18:51 +0000

    ✨prettier✨

--- db-service/lib/cqn4sql.js
+++ db-service/lib/cqn4sql.js
@@ -573,7 +573,11 @@ function cqn4sql(query, model = cds.context?.model || cds.model) {
       const { index, tableAlias } = inferred.$combinedElements[k][0]
       const element = tableAlias.elements[k]
       // ignore FK for odata csn / ignore blobs from wildcard expansion
-      if (isODataFlatForeignKey(element) || (element.type === 'cds.LargeBinary' && element['@Core.MediaType'] !== undefined)) return
+      if (
+        isODataFlatForeignKey(element) ||
+        (element.type === 'cds.LargeBinary' && element['@Core.MediaType'] !== undefined)
+      )
+        return
       const flatColumns = getFlatColumnsFor(element, null, null, index, [], except, replace)
       wildcardColumns.push(...flatColumns)
     })
