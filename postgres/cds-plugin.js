ed97b5bc5db9c1db048c1cc8b4ab37eeb578d3c8
Sebastian Esch <sebastian.esch@ibsolution.de>
2023-06-02 08:27:51 +0000

    ✨prettier✨

--- postgres/cds-plugin.js
+++ postgres/cds-plugin.js
@@ -1,7 +1,7 @@
 const cds = require('@sap/cds/lib')
 
 cds.once('bootstrap', () => {
-    if (cds.requires.db?.impl === '@cap-js/postgres') {
-        cds.requires.db.vcap = { label: 'postgresql-db', tag: 'postgresql-db' }
-    }
+  if (cds.requires.db?.impl === '@cap-js/postgres') {
+    cds.requires.db.vcap = { label: 'postgresql-db', tag: 'postgresql-db' }
+  }
 })
