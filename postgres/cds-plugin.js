225c4d329e5acd84d0892555969d36fb2974d2dc
Sebastian Esch <sebastian.esch@ibsolution.de>
2023-06-02 11:03:15 +0200

    Move config to package.json / update beershop package.json

--- postgres/cds-plugin.js
+++
@@ -1,7 +0,0 @@
-const cds = require('@sap/cds/lib')
-
-cds.once('bootstrap', () => {
-    if (cds.requires.db?.impl === '@cap-js/postgres') {
-        cds.requires.db.vcap = { label: 'postgresql-db', tag: 'postgresql-db' }
-    }
-})
