6fd5a9b9e2a1d9203051d174fa87aef81e28fa34
Simon Oswald <simon.oswald@sap.com>
2023-06-13 15:54:08 +0200

    Add some todos

--- postgres/lib/PostgresService.js
+++ postgres/lib/PostgresService.js
@@ -51,6 +51,7 @@ class PostgresService extends SQLService {
   }
 
   url4() {
+    // TODO: Maybe log which database and which user? Be more robust against missing properties?
     let { host, port } = this.options?.credentials || this.options || {}
     return 'postgres@' + host + ':' + (port || 5432)
   }
