3caf6aa3bdd54750fee4821ee6b5aac2ad05544e
Johannes Vogel <31311694+johannes-vogel@users.noreply.github.com>
2023-06-15 16:42:53 +0200

    restore output converters
    required for stakeholder tests

--- sqlite/lib/SQLiteService.js
+++ sqlite/lib/SQLiteService.js
@@ -100,6 +100,11 @@ class SQLiteService extends SQLService {
       Double: expr => `nullif(quote(${expr}),'NULL')->'$'`,
       struct: expr => `${expr}->'$'`, // Association + Composition inherits from struct
       array: expr => `${expr}->'$'`,
+      // REVISIT: Timestamp should not loos precision
+      Date: e => `strftime('%Y-%m-%d',${e})`,
+      Time: e => `strftime('%H:%M:%S',${e})`,
+      DateTime: e => `strftime('%Y-%m-%dT%H:%M:%SZ',${fixTimeZone(e)})`,
+      Timestamp: e => `strftime('%Y-%m-%dT%H:%M:%fZ',${fixTimeZone(e)})`
     }
 
     // Used for SQL function expressions
