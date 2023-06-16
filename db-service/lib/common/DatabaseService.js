119a01f1ae0fe2d7d3003f5057ee20239d3c2a4f
Daniel Hutzel <daniel.hutzel@sap.com>
2023-06-16 10:39:09 +0000

    ✨prettier✨

--- db-service/lib/common/DatabaseService.js
+++ db-service/lib/common/DatabaseService.js
@@ -9,7 +9,6 @@ function Pool(factory, tenant) {
 const { createPool } = require('@sap/cds-foss').pool
 
 class DatabaseService extends cds.Service {
-
   /**
    * Return a pool factory + options property as expected by
    * https://github.com/coopernurse/node-pool#createpool.
