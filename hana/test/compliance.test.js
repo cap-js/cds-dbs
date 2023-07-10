ffeb3f410ae880dded71e47b32563be2a5af3050
Bob den Os <bob.den.os@sap.com>
2023-07-10 12:53:38 +0200

    Don't restart prepared HXE in actions

--- hana/test/compliance.test.js
+++ hana/test/compliance.test.js
@@ -1,5 +1,3 @@
 describe('hana', () => {
-  require('../../test/compliance')
-  require('../../test/scenarios/bookshop')
-  require('../../test/scenarios/sflight/read.test')
+  require('../../test')
 })
--- hana/tools/docker/hxe/start.sh
+++ hana/tools/docker/hxe/start.sh
@@ -2,8 +2,10 @@ exists=$(docker images hanaexpress:current -q);
 if [ $exists ]; then
     echo "Using prepared HXE image"
     docker-compose -f ci.yml up -d;
+    docker exec hxe_hana_1 bash -c "until /check_hana_health -n -e ready-status > /dev/null; do sleep 1; done;"
+    echo "HANA has started"
 else
     export VERSION=$(node ./latest.js);
     docker-compose -f hana.yml up -d;
+    ./ready.sh;
 fi
-./ready.sh;
