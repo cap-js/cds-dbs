75ae070c16cc5a3693864fdfddf8a037799b868c
Bob den Os <bob.den.os@sap.com>
2023-07-11 17:19:12 +0200

    Call start-hdi after boot again

--- hana/tools/docker/hxe/start.sh
+++ hana/tools/docker/hxe/start.sh
@@ -8,6 +8,9 @@ if [ $exists ]; then
     docker-compose -f ci.yml up -d;
     docker exec hxe_hana_1 bash -c "until /check_hana_health -n -e ready-status > /dev/null; do sleep 1; done;"
     echo "HANA has started"
+    ## REVISIT: Remove this when it works properly in the docker build step
+    docker exec hxe_hana_1 bash -c "/usr/sap/HXE/HDB90/exe/hdbsql -i 90 -d SYSTEMDB -u SYSTEM -p Manager1 -I /usr/sap/HXE/start-hdi.sql > /dev/null"
+    echo "HDI has been enabled"
 else
     export VERSION=$(node ./latest.js);
     docker-compose -f hana.yml up -d;
