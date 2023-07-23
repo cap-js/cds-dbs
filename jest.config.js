1543465af9da46ac5762b3abe615ee377a545063
Daniel Hutzel <daniel.hutzel@sap.com>
2023-07-23 16:56:36 +0200

    Using default jest reporters only

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  reporters: process.env.CI ? ['github-actions', 'summary'] : ['default'],
-  testPathIgnorePatterns: [
-    'test/scenarios/sflight/integration.test.js', // REVISIT: is that test being run anywhere?
-    'postgres',
-  ]
-}
+exports.testPathIgnorePatterns = [
+  'test/scenarios/sflight/integration.test.js', // REVISIT: is that test being run anywhere?
+  'postgres',
+]
+
+// REVISIT: What do these reporters give us?
+// if (process.env.CI) exports.reporters = ['github-actions', 'summary']
