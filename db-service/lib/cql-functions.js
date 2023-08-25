3f063b9b328c22c92f1eac6002bc33087ff7dd89
Daniel Hutzel <daniel.hutzel@sap.com>
2023-08-25 12:15:51 +0200

    Use session_context() with literals ... instead of binding parameters

--- db-service/lib/cql-functions.js
+++ db-service/lib/cql-functions.js
@@ -1,4 +1,8 @@
 const StandardFunctions = {
+
+
+  session_context: x => `session_context('${x.val}')`,
+
   // OData: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_CanonicalFunctions
 
   // String and Collection Functions
