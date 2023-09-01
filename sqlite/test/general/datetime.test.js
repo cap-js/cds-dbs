5c43d5c5ef8e298e6ed70a274b439b10aae5f339
Johannes Vogel <johannes.vogel@sap.com>
2023-09-01 12:48:37 +0200

    reproduce issue with timestamp as key

---
+++ sqlite/test/general/datetime.test.js
@@ -0,0 +1,14 @@
+const cds = require('../../../test/cds.js')
+
+cds.test(__dirname, 'testModel.cds')
+
+describe('datetime handling', () => {
+  test('datetime elements as key', async () => {
+    let res
+    const payload = { dt: '2020-12-31T01:02:03Z', int: 4711}
+    res = await cds.db.run(INSERT.into('DateTimeEntity').entries(payload))
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: payload.dt})) // this returns undefined
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: '2020-12-31T01:02:03.000Z'})) // this finds the value but shouldn't
+    expect(res).toMatchObject(payload)
+  })
+})
