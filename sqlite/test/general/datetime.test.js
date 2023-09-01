7a86b10c18340351041064b812aa7763cbfdc922
sjvans <30337871+sjvans@users.noreply.github.com>
2023-09-01 13:25:59 +0200

    Update sqlite/test/general/datetime.test.js

--- sqlite/test/general/datetime.test.js
+++ sqlite/test/general/datetime.test.js
@@ -7,7 +7,7 @@ describe('datetime handling', () => {
     let res
     const payload = { dt: '2020-12-31T01:02:03Z', int: 4711}
     res = await cds.db.run(INSERT.into('DateTimeEntity').entries(payload))
-    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(dt).toISOString() })) // this finds the value
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(payload .dt).toISOString() })) // this finds the value
     expect(res).toMatchObject(payload)
     res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: payload.dt})) // this returns undefined
     expect(res).toMatchObject(payload)
