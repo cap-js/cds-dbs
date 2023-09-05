8175e00c456cf77793e6db9b39684399e58bb40d
Johannes Vogel <31311694+johannes-vogel@users.noreply.github.com>
2023-09-05 11:27:14 +0200

    reproduce issue with timestamp as key (#206)
    * reproduce issue with timestamp as key
    * Fix w/ regex
    
    ---------
    Co-authored-by: sjvans <30337871+sjvans@users.noreply.github.com>
    Co-authored-by: Daniel Hutzel <daniel.hutzel@sap.com>

---
+++ sqlite/test/general/datetime.test.js
@@ -0,0 +1,17 @@
+const cds = require('../../../test/cds.js')
+
+cds.test(__dirname, 'testModel.cds')
+
+describe('datetime handling', () => {
+  test('datetime elements as key', async () => {
+    let res
+    const payload = { dt: '2020-12-31T01:02:03Z', int: 4711}
+    res = await cds.db.run(INSERT.into('DateTimeEntity').entries(payload))
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(payload .dt).toISOString() }))
+    expect(res).toMatchObject(payload)
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(payload .dt).toISOString() }))
+    expect(res).toMatchObject(payload)
+    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: payload.dt}))
+    expect(res).toMatchObject(payload)
+  })
+})
