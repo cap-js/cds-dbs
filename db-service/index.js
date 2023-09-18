a6c4ff16be1fcd6a5e768f2c7b993a9379325e91
Daniel Hutzel <daniel.hutzel@sap.com>
2023-09-18 13:24:33 +0200

    Less Typescript artefacts

--- db-service/index.js
+++ db-service/index.js
@@ -1,15 +1,6 @@
 const DatabaseService = require('./lib/common/DatabaseService')
 const SQLService = require('./lib/SQLService')
-const CQN2SQL = require('./lib/cqn2sql').classDefinition
-
-/**
- * @template T
- * @typedef {import('./lib/common/factory').Factory<T>} Factory
- */
-
-/**
- * @typedef {import('./lib/SQLService').prototype.PreparedStatement} PreparedStatement
- */
+const CQN2SQL = require('./lib/cqn2sql').class
 
 module.exports = {
   DatabaseService,
