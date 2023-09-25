7780ee3e5c9e99af30e1486e45c4f3ab8b49e4d4
Steffen Waldmann <steffenwaldmann@me.com>
2023-09-25 12:53:52 +0200

    Use `@sap/cds/lib`, as before

--- postgres/cds-plugin.js
+++ postgres/cds-plugin.js
@@ -1,4 +1,4 @@
-const cds = require('@sap/cds')
+const cds = require('@sap/cds/lib')
 
 if (!cds.env.fiori.lean_draft) {
   throw new Error('"@cap-js/postgres" only works if cds.fiori.lean_draft is enabled. Please adapt your configuration.')
