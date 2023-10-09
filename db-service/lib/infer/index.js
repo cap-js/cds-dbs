b06b4d7299dae0d2f2478812cf100627a1b9644f
Patrice Bender <patrice.bender@sap.com>
2023-10-09 13:27:36 +0200

    db-service: enable anonymous expand on (implicit) table alias
    enable queries like:
    
    ```
    select from Books { { * } as FOO }
    select from Books as Bar { Bar { * } }
    ```

--- db-service/lib/infer/index.js
+++ db-service/lib/infer/index.js
@@ -173,7 +173,9 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
             const nextStep = ref[1]?.id || ref[1]
             // no unmanaged assoc in infix filter path
             if (!expandOrExists && e.on)
-              throw new Error(`"${e.name}" in path "${arg.ref.map(idOnly).join('.')}" must not be an unmanaged association`)
+              throw new Error(
+                `"${e.name}" in path "${arg.ref.map(idOnly).join('.')}" must not be an unmanaged association`,
+              )
             // no non-fk traversal in infix filter
             if (!expandOrExists && nextStep && !(nextStep in e.foreignKeys))
               throw new Error(`Only foreign keys of "${e.name}" can be accessed in infix filter`)
@@ -339,13 +341,11 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
     }
 
     // walk over all paths in other query properties
-    if (where)
-      walkTokenStream(where)
-    if (groupBy)
-      groupBy.forEach(token => inferQueryElement(token, false))
-    if (having)
-      walkTokenStream(having)
-    if (_.with) // consider UPDATE.with
+    if (where) walkTokenStream(where)
+    if (groupBy) groupBy.forEach(token => inferQueryElement(token, false))
+    if (having) walkTokenStream(having)
+    if (_.with)
+      // consider UPDATE.with
       Object.values(_.with).forEach(val => inferQueryElement(val, false))
 
     return queryElements
@@ -355,8 +355,8 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
      * on the information whether the next token is resolved within an `exists` predicates.
      * If such a token has an infix filter, it is not join relevant, because the filter
      * condition is applied to the generated `exists <subquery>` condition.
-     * 
-     * @param {array} tokenStream 
+     *
+     * @param {array} tokenStream
      */
     function walkTokenStream(tokenStream) {
       let skipJoins
@@ -479,7 +479,7 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
       const firstStepIsTableAlias =
         (column.ref.length > 1 && column.ref[0] in sources) ||
         // nested projection on table alias
-        (column.ref.length === 1 && column.ref[0] in sources && column.inline)
+        (column.ref.length === 1 && column.ref[0] in sources && (column.inline || column.expand))
       const firstStepIsSelf =
         !firstStepIsTableAlias && column.ref.length > 1 && ['$self', '$projection'].includes(column.ref[0])
       const nameSegments = []
@@ -545,9 +545,9 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
 
           if (firstStepIsSelf && element?.isAssociation) {
             throw new Error(
-              `Paths starting with “$self” must not contain steps of type “cds.Association”: ref: [ ${column.ref.map(
-                idOnly,
-              ).join(', ')} ]`,
+              `Paths starting with “$self” must not contain steps of type “cds.Association”: ref: [ ${column.ref
+                .map(idOnly)
+                .join(', ')} ]`,
             )
           }
 
@@ -619,7 +619,12 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
             if (insertIntoQueryElements) queryElements[column.as || flatName] = getCopyWithAnnos(column, base)
           } else if (column.expand) {
             const elements = resolveExpand(column)
-            if (insertIntoQueryElements) queryElements[column.as || flatName] = elements
+            let elementName
+            // expand on table alias
+            if (column.$refLinks.length === 1 && column.$refLinks[0].definition.kind === 'entity')
+              elementName = column.$refLinks[0].alias
+            else elementName = column.as || flatName
+            if (insertIntoQueryElements) queryElements[elementName] = elements
           } else if (column.inline && insertIntoQueryElements) {
             const elements = resolveInline(column)
             queryElements = { ...queryElements, ...elements }
@@ -752,8 +757,8 @@ function infer(originalQuery, model = cds.context?.model || cds.model) {
        */
       function resolveExpand(col) {
         const { expand, $refLinks } = col
-        const $leafLink = $refLinks?.[$refLinks.length - 1]
-        if ($leafLink?.definition._target) {
+        const $leafLink = $refLinks?.[$refLinks.length - 1] || inferred.SELECT.from.$refLinks.at(-1) // fallback to anonymous expand
+        if ($leafLink.definition._target) {
           const expandSubquery = {
             SELECT: {
               from: $leafLink.definition._target.name,
