da588aac0498f6691e4b6ea99560c35255375ba3
Steffen Waldmann <steffen.waldmann@sap.com>
2023-10-23 17:09:44 +0200

    Require from `@sap/cds` directly

--- postgres/cds-plugin.js
+++ postgres/cds-plugin.js
@@ -1,10 +1,10 @@
-const cds = require('@sap/cds-dk/lib')
+const cds = require('@sap/cds')
 
 if (!cds.env.fiori.lean_draft) {
   throw new Error('"@cap-js/postgres" only works if cds.fiori.lean_draft is enabled. Please adapt your configuration.')
 }
 
-cds.build.register('postgres', {
+cds.build?.register?.('postgres', {
   impl: __dirname + '/lib/build.js',
   taskDefaults: { src: cds.env.folders.db, dest: 'pg' }
 })
