fcd643efc6529a38c33d389400cd331e0953c541
Patrice Bender <patrice.bender@sap.com>
2023-10-31 12:18:04 +0100

    add end2end test with renamed keys

--- test/compliance/DELETE.test.js
+++ test/compliance/DELETE.test.js
@@ -1,4 +1,11 @@
+'use strict'
+
+const cds = require('../cds.js')
+const assert = require('assert')
+
 describe('DELETE', () => {
+  cds.test(__dirname + '/../bookshop')
+
   describe('from', () => {
     test.skip('missing', () => {
       throw new Error('not supported')
@@ -6,8 +13,16 @@ describe('DELETE', () => {
   })
 
   describe('where', () => {
-    test.skip('missing', () => {
-      throw new Error('not supported')
+    test.skip('path expressions', async () => {
+      const deleteEmilysBooks = DELETE.from('AdminService.RenameKeys').where(`author.name = 'Emily Brontë'`)
+      const selectEmilysBooks = CQL`SELECT * FROM sap.capire.bookshop.Books where author.name = 'Emily Brontë'`
+
+      const beforeDelete = await cds.run(selectEmilysBooks)
+      await cds.run(deleteEmilysBooks)
+      const afterDelete = await cds.run(selectEmilysBooks)
+
+      expect(beforeDelete).toHaveLength(1)
+      expect(afterDelete).toHaveLength(0)
     })
   })
 })
