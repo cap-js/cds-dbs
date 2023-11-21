3cfd03a7d9c188ee9630d7c9ef1c3e985d724e7d
Samuel Brucksch <samuel.brucksch@sap.com>
2023-11-21 14:54:21 +0100

    revert accidental change

--- postgres/lib/PostgresService.js
+++ postgres/lib/PostgresService.js
@@ -485,7 +485,7 @@ GROUP BY k
         await this.tx(async tx => {
           // await tx.run(`DROP USER IF EXISTS "${creds.user}"`)
           await tx
-            .run(`CREATE USER "${creds.user}" PASSWORD '${creds.user}'`)
+            .run(`CREATE USER "${creds.user}" PASSWORD '${creds.password}'`)
             .catch(e => {
               if (e.code === '42710') return
               throw e
