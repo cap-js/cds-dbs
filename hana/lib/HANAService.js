1ae840e51a96b2660b1ff9f3fa788eae46252a37
Vitaly Kozyura <v.kozyura@sap.com>
2024-01-03 11:32:03 +0100

    add readable to hana

--- hana/lib/HANAService.js
+++ hana/lib/HANAService.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const  { Readable } = require('stream')
 
 const { SQLService } = require('@cap-js/db-service')
 const drivers = require('./drivers')
