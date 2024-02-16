e1e0faef2dd21b603f0de262687d88707b8564eb
Johannes Vogel <johannes.vogel@sap.com>
2024-02-16 15:22:30 +0100

    .

--- test/scenarios/bookshop/insert.test.js
+++ test/scenarios/bookshop/insert.test.js
@@ -2,15 +2,16 @@ const cds = require('../../cds.js')
 const bookshop = cds.utils.path.resolve(__dirname, '../../bookshop')
 
 describe('Bookshop - Insert', () => {
-  const { expect } = cds.test(bookshop)
+  const { expect: expect1 } = cds.test(bookshop)
 
   test('unique constraing violation throws error', async () => {
     const { Books } = cds.entities('AdminService')
     const insert = INSERT({ ID: 201, title: 'Harry Potter' }).into(Books)
-    const err = await expect(insert).rejected
+    const err = await expect1(insert).rejected
     // Works fine locally, but refuses to function in pipeline
-    expect(err).to.be.instanceOf(Error)
-    expect(err instanceof Error).to.be.true
-    expect(err.message).to.be.eq('ENTITY_ALREADY_EXISTS')
+    expect(err).toBeInstanceOf(Error)
+    expect1(err).to.be.instanceOf(Error)
+    expect1(err instanceof Error).to.be.true
+    expect1(err.message).to.be.eq('ENTITY_ALREADY_EXISTS')
   })
 })
