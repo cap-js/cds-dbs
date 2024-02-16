a65ccf08b670f9f49e0df15e1cccc84f863a12aa
Johannes Vogel <johannes.vogel@sap.com>
2024-02-16 15:13:04 +0100

    .

--- test/x.test.js
+++ test/x.test.js
@@ -1,20 +1,22 @@
 test('bs error', async () => {
-
   const bs = require('better-sqlite3')
   const dbc = new bs(':memory:')
   await dbc.exec('CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)')
-  await dbc.exec('INSERT INTO test (id,name) VALUES (12, \'test\')')
+  await dbc.exec("INSERT INTO test (id,name) VALUES (12, 'test')")
   try {
-    await dbc.exec('INSERT INTO test (id,name) VALUES (12, \'test\')')
-  } catch (err) {
-    const cdsErr = Object.assign(err, {
-      originalMessage: err.message,
-      message: 'asdf',
-      code: 400
-    })
-    const  {VError} = require('verror')
-    var err2 = new VError(cdsErr, 'sdfsd');
-    console.error(err2.message);
-    expect(cdsErr).toBeInstanceOf(Error)
+    try {
+      await dbc.exec("INSERT INTO test (id,name) VALUES (12, 'test')")
+    } catch (err) {
+      throw Object.assign(err, {
+        originalMessage: err.message,
+        message: 'asdf',
+        code: 400,
+      })
+    }
+  } catch (e) {
+    const { VError } = require('verror')
+    var err2 = new VError(e, 'sdfsd')
+    console.error(err2.message)
+    expect(e).toBeInstanceOf(Error)
   }
 })
