b74aa4e7ae6dadab408d9a69c69384302f1470d4
Johannes Vogel <johannes.vogel@sap.com>
2024-02-16 15:17:53 +0100

    .

--- test/scenarios/bookshop/insert.test.js
+++ test/scenarios/bookshop/insert.test.js
@@ -9,8 +9,8 @@ describe('Bookshop - Insert', () => {
     const insert = INSERT({ ID: 201, title: 'Harry Potter' }).into(Books)
     const err = await expect(insert).rejected
     // Works fine locally, but refuses to function in pipeline
-    // expect(err).to.be.instanceOf(Error)
-    // expect(err instanceof Error).to.be.true
+    expect(err).to.be.instanceOf(Error)
+    expect(err instanceof Error).to.be.true
     expect(err.message).to.be.eq('ENTITY_ALREADY_EXISTS')
   })
 })
