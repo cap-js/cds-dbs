a055f9fc9f282d4c851be0a7a5c8e8562bf9af41
Daniel Hutzel <daniel.hutzel@sap.com>
2024-03-05 13:02:05 +0100

    Revert "fix: deep delete for views without navigation (#434)"
    This reverts commit 3ebc9c29362045916c9060528729320c0eae4391.

--- db-service/lib/SQLService.js
+++ db-service/lib/SQLService.js
@@ -1,7 +1,7 @@
 const cds = require('@sap/cds/lib'),
   DEBUG = cds.debug('sql|db')
 const { Readable } = require('stream')
-const { resolveView, getDBTable, getTransition } = require('@sap/cds/libx/_runtime/common/utils/resolveView')
+const { resolveView } = require('@sap/cds/libx/_runtime/common/utils/resolveView')
 const DatabaseService = require('./common/DatabaseService')
 const cqn4sql = require('./cqn4sql')
 
@@ -25,18 +25,16 @@ class SQLService extends DatabaseService {
     this.on(['INSERT', 'UPSERT', 'UPDATE'], require('./deep-queries').onDeep)
     if (cds.env.features.db_strict) {
       this.before(['INSERT', 'UPSERT', 'UPDATE'], ({ query }) => {
-        const elements = query.target?.elements
-        if (!elements) return
+        const elements = query.target?.elements; if (!elements) return
         const kind = query.kind || Object.keys(query)[0]
         const operation = query[kind]
         if (!operation.columns && !operation.entries && !operation.data) return
         const columns =
           operation.columns ||
           Object.keys(
-            operation.data ||
-              operation.entries?.reduce((acc, obj) => {
-                return Object.assign(acc, obj)
-              }, {}),
+            operation.data || operation.entries?.reduce((acc, obj) => {
+              return Object.assign(acc, obj)
+            }, {}),
           )
         const invalidColumns = columns.filter(c => !(c in elements))
 
@@ -117,11 +115,7 @@ class SQLService extends DatabaseService {
    */
   async onSELECT({ query, data }) {
     if (!query.target) {
-      try {
-        this.infer(query)
-      } catch (e) {
-        /**/
-      }
+      try { this.infer(query) } catch (e) { /**/ }
     }
     if (query.target && !query.target._unresolved) {
       // Will return multiple rows with objects inside
@@ -201,38 +195,11 @@ class SQLService extends DatabaseService {
     return (await ps.run(values)).changes
   }
 
-  exists(e) {
-    return e && !e.virtual && !e.value && !e.isAssociation
-  }
-
   get onDELETE() {
     // REVISIT: It's not yet 100 % clear under which circumstances we can rely on db constraints
     return (super.onDELETE = /* cds.env.features.assert_integrity === 'db' ? this.onSIMPLE : */ deep_delete)
     async function deep_delete(/** @type {Request} */ req) {
-      const transitions = getTransition(req.query.target, this)
-      if (transitions.target !== transitions.queryTarget) {
-        const keys = []
-        const transitionsTarget = transitions.queryTarget.keys || transitions.queryTarget.elements
-        for (const key in transitionsTarget) {
-          if (this.exists(transitionsTarget[key])) keys.push(key)
-        }
-        const matchedKeys = keys.filter(key => transitions.mapping.has(key)).map(k => ({ ref: [k] }))
-        const query = DELETE.from({
-          ref: [
-            {
-              id: transitions.target.name,
-              where: [
-                { list: matchedKeys.map(k => transitions.mapping.get(k.ref[0])) },
-                'in',
-                SELECT.from(req.query.DELETE.from).columns(matchedKeys).where(req.query.DELETE.where),
-              ],
-            },
-          ],
-        })
-        return this.onDELETE({ query })
-      }
-      const table = getDBTable(req.query.target)
-      const { compositions } = table
+      let { compositions } = req.target
       if (compositions) {
         // Transform CQL`DELETE from Foo[p1] WHERE p2` into CQL`DELETE from Foo[p1 and p2]`
         let { from, where } = req.query.DELETE
@@ -244,22 +211,22 @@ class SQLService extends DatabaseService {
         }
         // Process child compositions depth-first
         let { depth = 0, visited = [] } = req
-        visited.push(req.query.target.name)
+        visited.push(req.target.name)
         await Promise.all(
           Object.values(compositions).map(c => {
             if (c._target['@cds.persistence.skip'] === true) return
-            if (c._target === req.query.target) {
+            if (c._target === req.target) {
               // the Genre.children case
               if (++depth > (c['@depth'] || 3)) return
             } else if (visited.includes(c._target.name))
               throw new Error(
                 `Transitive circular composition detected: \n\n` +
-                  `  ${visited.join(' > ')} > ${c._target.name} \n\n` +
-                  `These are not supported by deep delete.`,
+                `  ${visited.join(' > ')} > ${c._target.name} \n\n` +
+                `These are not supported by deep delete.`,
               )
             // Prepare and run deep query, à la CQL`DELETE from Foo[pred]:comp1.comp2...`
             const query = DELETE.from({ ref: [...from.ref, c.name] })
-            return this.onDELETE({ query, depth, visited: [...visited] })
+            return this.onDELETE({ query, depth, visited: [...visited], target: c._target })
           }),
         )
       }
