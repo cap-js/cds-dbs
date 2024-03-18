a64e2fc234d3c9b0f406ab75f213df449712eb7b
Bob den Os <bob.den.os@sap.com>
2024-03-18 23:11:52 +0100

    Add simple scenario with measurements

--- db-fractal/lib/FractalService.js
+++ db-fractal/lib/FractalService.js
@@ -11,6 +11,12 @@ class FractalService extends DatabaseService {
 
     this._source = cds.connect.to(this.options.source)
     this._cache = cds.connect.to(this.options.cache)
+      .then(async cache => {
+        // REVISIT: deploy without default data (unless garanteed static)
+        // Prepare model in the cache service for faster cold cache access
+        await cache.tx(tx => cds.deploy(cds.options.from).to(tx))
+        return cache
+      })
 
     this._tokens = {}
 
@@ -67,7 +73,8 @@ class FractalService extends DatabaseService {
 
   }
 
-  // Should be handeled by instance 0
+  // REVISIT: Forward token grant requests to instance(0)
+  // REVISIT: Grant tokens based upon source tables
   async grantToken(req, resource) {
     await new Promise(resolve => setTimeout(resolve, 1))
     const tokens = resource.tokens * -1
@@ -76,13 +83,13 @@ class FractalService extends DatabaseService {
     return {
       id: resource.id,
       // 'source', 'cache', 'instance(n)'
-      target: tokens > 1 && process.env.FRACT_CACHE ? 'cache' : 'source',
+      target: tokens > 1 && process.env.FRACTAL_CACHE !== 'false' ? 'cache' : 'source',
       tokens: tokens,
       validTo: validTo.getTime(),
     }
   }
 
-  async getToken(req) {
+  async getToken(req, retry = false) {
     const { tenant, target } = req
     const key = `${tenant}:${target}`
     const defaultResource = { id: key, tokens: 0 }
@@ -90,32 +97,71 @@ class FractalService extends DatabaseService {
 
     // If the resouce is no longer valid
     if (resource.validTo < Date.now()) {
+      await this.flushCache(req, resource)
       this._tokens[key] = defaultResource
       return this.getToken(req)
     }
     // If the target is the local cache ignore tokens
-    if (resource.target === 'cache') return resource
+    if (resource.target === 'cache') {
+      if (req.event !== 'SELECT') resource.changed = true
+      return resource
+    }
     // Consume available token
-    if (resource.tokens-- > 0) return resource
+    if (!retry && resource.tokens-- > 0) return resource
     // Queue current request to existing update request
-    if (resource.prom) return resource.prom
+    if (resource.prom && !retry) return resource.prom
     // Intialize new resource tokens request
     return (resource.prom = this.grantToken(req, resource)
       .then(async newResource => {
         newResource.tokens += resource.tokens
         this._tokens[key] = newResource
         if (newResource.target === 'cache') {
-          try {
-            await (await this._cache).tx(tx => {
-              return cds.deploy(cds.options.from).to(tx)
-            })
-          } catch (e) {
-            debugger
-          }
+          await this.getCache(req, resource)
         }
         return newResource
       })
+      // REVISIT: Make sure that all token requests resolve
+      .catch(() => this.getToken(req, true))
     )
+  }
+
+  async getCache(req, resource) {
+    try {
+      const cache = await this._cache
+      const source = await this._source
+      // Update local cache with the current state of the source database
+      await cache.tx(async tx => tx.run(
+        UPSERT(await source.tx(tx => tx.run(SELECT.from(req.target))))
+          .into(req.target)
+      ))
+    } catch (e) {
+      delete this._tokens[resource.id]
+    }
+  }
+
+  async flushCache(req, resource) {
+    if (!resource.changed) return // No write operations so no flush
+    const cache = await this._cache
+    const source = await this._source
+    await source.tx(async tx => {
+      const targetState = await cache.tx(tx => tx.run(SELECT.from(req.target)))
+
+      // Delete all entries which no longer exist in the cache
+      // REVISIT: this approach works, but also sends all data an additional time
+      //          Could be more efficient by tracking the DELETE clauses
+      const elements = Object.keys(req.target.keys ?? req.target.elements)
+      await tx.run(DELETE.from(req.target).where([
+        'not',
+        { list: elements.map(k => ({ ref: [k] })) },
+        'in',
+        { list: targetState.map(row => ({ list: elements.map(e => ({ val: row[e] })) })) }
+      ]))
+
+      // UPSERT the current state of the cache into the source database
+      // REVISIT: this is a very straight forward approach. Could be optimized.
+      //          By tracking the INSERT, UPSERT and UPDATE queries and filter
+      return tx.run(UPSERT(targetState).into(req.target))
+    })
   }
 
   // queue queries until tokens are granted
@@ -129,6 +175,11 @@ class FractalService extends DatabaseService {
 
   async onPlainSQL(req, next) {
     return (await this.source).onPlainSQL(req, next)
+  }
+
+  async disconnect(tenant) {
+    const [source, cache] = await Promise.all([this._source, this._cache])
+    await Promise.all([source.disconnect(tenant), cache.disconnect(tenant)])
   }
 
   async database(isolate) {
--- db-fractal/test/stress.test.js
+++ db-fractal/test/stress.test.js
@@ -2,6 +2,9 @@ const cds = require('../../test/cds')
 
 const bookshop = cds.utils.path.resolve(__dirname, '../../test/bookshop')
 
+// Cache toggle
+process.env.FRACTAL_CACHE = 'false'
+
 describe('stress', () => {
   beforeAll(() => {
     cds.requires.postgres = require('../../postgres/test/service.json')
@@ -10,26 +13,45 @@ describe('stress', () => {
 
   const { expect, GET } = cds.test(bookshop)
 
-  test('test', async () => {
-    try {
-      const { Books } = cds.entities('sap.capire.bookshop')
-      let start, queries
-
-      // Cold run includes creating cache
-      queries = new Array(100).fill('').map(() => SELECT.from(Books))
-      start = performance.now()
-      await Promise.all(queries)
-      // cache: ~160ms no-cache: ~270ms
-      console.log('cold', performance.now() - start, 'ms')
-
-      // Hot run already has cache created
-      queries = new Array(100).fill('').map(() => SELECT.from(Books))
-      start = performance.now()
-      await Promise.all(queries)
-      // cache: ~40ms no-cache: ~270ms
-      console.log('hot', performance.now() - start, 'ms')
-    } catch (err) {
-      debugger
-    }
+  beforeEach(async () => {
+    await cds.disconnect()
+    delete cds.services.db
+    await cds.deploy(cds.options.from).to('db')
   })
+
+  test.each([1, 10, 100, 1000])('test', async (queryCount) => {
+    const { Books } = cds.entities('sap.capire.bookshop')
+    let start, queries
+
+    process.stdout.write(`queries : ${queryCount}\n`)
+
+    // Apply write operation to source database
+    await INSERT({ ID: 999 }).into(Books)
+
+    // Cold run includes creating cache
+    queries = new Array(queryCount).fill('').map(() => SELECT.from(Books))
+    start = performance.now()
+    await Promise.all(queries)
+    // cache: ~160ms no-cache: ~270ms
+    process.stdout.write(`cold :${(`${(((performance.now() - start) * 100) >>> 0) / 100}`.padStart(7, ' '))}ms\n`)
+
+    // Hot run already has cache created
+    queries = new Array(queryCount).fill('').map(() => SELECT.from(Books))
+    start = performance.now()
+    await Promise.all(queries)
+    // cache: ~40ms no-cache: ~270ms
+    process.stdout.write(` hot :${(`${(((performance.now() - start) * 100) >>> 0) / 100}`.padStart(7, ' '))}ms\n`)
+
+    // Apply write operations to local cache
+    await DELETE(Books).where({ ID: 201 })
+    await INSERT({ ID: 998 }).into(Books)
+    await UPDATE(Books).with({ descr: 'UPDATED' }).where({ ID: 251 })
+    const cache = await SELECT.from(Books)
+
+    // Invalidate Books cache token
+    cds.db._tokens[`${undefined}:${SELECT.from(Books).target}`].validTo = 0
+    const source = await SELECT.from(Books)
+
+    expect(source).to.deep.equal(cache)
+  }, 30 * 1000)
 })
