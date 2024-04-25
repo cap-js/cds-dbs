374c81e6c74e2561ae30b47a8a925eebafc91604
D050513 <sebastian.van.syckel@sap.com>
2024-04-25 13:09:37 +0200

    fix

--- postgres/test/ql.test.js
+++ postgres/test/ql.test.js
@@ -63,7 +63,7 @@ describe('QL to PostgreSQL', () => {
           .where({ abv: { '>': 1.0 } })
           .orderBy({ abv: 'desc' }),
       )
-      expect(beers[0].abv).to.equal("5.9")
+      expect(beers[0].abv).to.equal(cds.env.features.string_decimals ? "5.9" : 5.9)
       const reverseBeers = await cds.run(
         SELECT.from(Beers)
           .where({ abv: { '>': 1.0 } })
