156695a3dbdf0aa461b4ad03a8810f6370a92ce9
Patrice Bender <patrice.bender@sap.com>
2024-06-12 11:54:25 +0200

    chore(lint): dont complain about unused errors in a catch

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ module.exports = [
     },
     rules: {
       'no-extra-semi': 'off',
-      'no-unused-vars': ['warn', { argsIgnorePattern: 'lazy' }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: 'lazy', caughtErrorsIgnorePattern: '^e$' }],
       'no-console': 'error',
     },
   },
