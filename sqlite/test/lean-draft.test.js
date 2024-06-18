071739684104034ac954b395a8d2c431a23f81a2
D050513 <sebastian.van.syckel@sap.com>
2024-06-18 13:25:59 +0200

    expect string decimals

--- sqlite/test/lean-draft.test.js
+++ sqlite/test/lean-draft.test.js
@@ -236,7 +236,7 @@ describe('draft tests', () => {
     expect(res.data.value.length).to.be.eq(1)
     expect(res.data.value[0]).to.containSubset({
       BeginDate: '2023-08-04',
-      BookingFee: 90,
+      BookingFee: '90',
       CurrencyCode_code: 'USD',
       Description: 'Vacation to USA',
       EndDate: '2024-05-31',
@@ -348,11 +348,11 @@ describe('draft tests', () => {
     expect(res.data.value.length).to.be.eq(1)
     expect(res.data.value[0]).to.containSubset({
       BeginDate: '2023-08-04',
-      BookingFee: 90,
+      BookingFee: '90',
       CurrencyCode_code: 'USD',
       Description: 'Vacation to USA',
       EndDate: '2024-05-31',
-      TotalPrice: 5624,
+      TotalPrice: '5624',
       TravelID: 32,
       TravelStatus_code: 'O',
       TravelUUID: EDIT_DRAFT_TRAVELUUID,
@@ -433,11 +433,11 @@ describe('draft tests', () => {
     expect(res.status).to.be.eq(200)
     expect(res.data.value[0]).to.containSubset({
       BeginDate: '2024-05-30',
-      BookingFee: 20,
+      BookingFee: '20',
       CurrencyCode_code: 'USD',
       Description: 'Sightseeing in New York City, New York',
       EndDate: '2024-05-30',
-      TotalPrice: 7375,
+      TotalPrice: '7375',
       TravelID: 4133,
       TravelStatus_code: 'A',
       TravelUUID: '76757221A8E4645C17002DF03754AB66',
@@ -496,11 +496,11 @@ describe('draft tests', () => {
     expect(res.status).to.be.eq(200)
     expect(res.data).to.containSubset({
       BeginDate: '2023-08-04',
-      BookingFee: 20,
+      BookingFee: '20',
       CurrencyCode_code: 'USD',
       Description: 'Business Trip for Christine, Pierre',
       EndDate: '2023-08-04',
-      TotalPrice: 900,
+      TotalPrice: '900',
       TravelID: 1,
       TravelStatus_code: 'O',
       TravelUUID: '52657221A8E4645C17002DF03754AB66',
@@ -555,7 +555,7 @@ describe('draft tests', () => {
       BookSupplUUID: '85D87221A8E4645C17002DF03754AB66',
       BookingSupplementID: 1,
       CurrencyCode_code: 'EUR',
-      Price: 20,
+      Price: '20',
       to_Supplement_SupplementID: 'ML-0023',
       to_Supplement: { Description: 'Trout Meuniere', SupplementID: 'ML-0023' },
       to_Travel: { TravelStatus: { code: 'A', fieldControl: 1 }, TravelUUID: '76757221A8E4645C17002DF03754AB66' },
@@ -577,7 +577,7 @@ describe('draft tests', () => {
       ConnectionID: '0018',
       CurrencyCode_code: 'USD',
       FlightDate: '2024-05-30',
-      FlightPrice: 3657,
+      FlightPrice: '3657',
       to_Carrier_AirlineID: 'GA',
       to_Customer_CustomerID: '000115',
       BookingStatus: { code: 'N', name: 'New' },
@@ -789,7 +789,7 @@ describe('draft tests', () => {
       { auth: { username: 'user1', password: 'user1' } },
     )
     expect(res.data).to.containSubset({
-      BookingFee: 12,
+      BookingFee: '12',
       TravelUUID,
       IsActiveEntity: false,
     })
@@ -815,7 +815,7 @@ describe('draft tests', () => {
       TravelID: 0,
       BeginDate: '2032-10-22',
       EndDate: '2032-12-22',
-      BookingFee: 12,
+      BookingFee: '12',
       TotalPrice: null,
       CurrencyCode_code: null,
       Description: null,
@@ -840,7 +840,7 @@ describe('draft tests', () => {
       '@odata.context':
         '$metadata#Travel(BeginDate,BookingFee,CurrencyCode_code,Description,EndDate,HasActiveEntity,HasDraftEntity,IsActiveEntity,TotalPrice,TravelID,TravelStatus_code,TravelUUID,to_Agency_AgencyID,to_Customer_CustomerID,DraftAdministrativeData(DraftIsCreatedByMe,DraftUUID,InProcessByUser),TravelStatus(code,createDeleteHidden,fieldControl,name),to_Agency(AgencyID,Name),to_Customer(CustomerID,LastName))/$entity',
       BeginDate: '2032-10-22',
-      BookingFee: 12,
+      BookingFee: '12',
       CurrencyCode_code: null,
       Description: null,
       EndDate: '2032-12-22',
