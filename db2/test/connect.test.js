9bca4ae531a9d363fbaae3347affcf132d1cdf92
Thanh <luffie.thanh@gmail.com>
2024-06-20 12:12:56 +0200

    connect to db2

--- db2/test/connect.test.js
+++ db2/test/connect.test.js
@@ -4,8 +4,16 @@ const ibmdb = require('ibm_db')
 const ConnStr = ``
 
 describe('DB2', function () {
-  describe('connect', function () {
-    cds.env.requires.db = require('@cap-js/db2/test/service.json')
-    console.log(cds.env)
+  describe('connect', async function () {
+    cds.env.requires.db = require('./service.json')
+    console.log('before connecting')
+    let conn = await ibmdb.open(
+      `DATABASE=testdb;HOSTNAME=localhost;UID=db2inst1;PWD=HariboMachtKinderFroh;PORT=50000;PROTOCOL=TCPIP`,
+    )
+    await conn.query('drop table mytab').catch(e => {
+      console.log(e)
+    })
+    await conn.query('create table mytab(c1 int, c2 varchar(10))')
+    console.log('🚀 ~ conn:', conn)
   })
 })
