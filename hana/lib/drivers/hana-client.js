601ef68ff6e5c74f4de26b3a4a39bdc87d87b5b0
Dr. David A. Kunz <david.kunz@sap.com>
2024-07-03 10:11:50 +0200

    add warning for missing config

--- hana/lib/drivers/hana-client.js
+++ hana/lib/drivers/hana-client.js
@@ -2,6 +2,7 @@ const { Readable, Stream } = require('stream')
 
 const hdb = require('@sap/hana-client')
 const { driver, prom, handleLevel } = require('./base')
+const LOG = cds.log('@sap/hana-client')
 
 const streamUnsafe = false
 
@@ -17,6 +18,7 @@ class HANAClientDriver extends driver {
    * @param {import('./base').Credentials} creds The credentials for the HANAClientDriver instance
    */
   constructor(creds) {
+    if (process.env.NODE_ENV === 'production' && !process.env.HDB_NODEJS_THREADPOOL_SIZE && !process.env.UV_THREADPOOL_SIZE) LOG.warn("When using @sap/hana-client, it's strongly recommended to adjust its threadpool size with environment variable `HDB_NODEJS_THREADPOOL_SIZE`, otherwise it might lead to performance issues.")
     // Enable native @sap/hana-client connection pooling
     creds = Object.assign({}, creds, {
       // REVISIT: add pooling related credentials when switching to native pools
