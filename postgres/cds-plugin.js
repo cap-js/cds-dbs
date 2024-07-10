37d8884cd6ecf907438a411a358e95383bf9dfbd
Thanh-NgHoL <luffie.thanh@gmail.com>
2024-07-10 18:18:10 +0200

    one test for db2 first attempt

--- postgres/cds-plugin.js
+++ postgres/cds-plugin.js
@@ -2,47 +2,51 @@ const cds = require('@sap/cds')
 const { fs, path } = cds.utils
 
 if (!cds.env.fiori.lean_draft) {
-  throw new Error('"@cap-js/postgres" only works if cds.fiori.lean_draft is enabled. Please adapt your configuration.')
+  throw new Error('"@quadrio/postgres" only works if cds.fiori.lean_draft is enabled. Please adapt your configuration.')
 }
 
 // requires @sap/cds-dk version >= 7.5.0
-cds.build?.register?.('postgres', class PostgresBuildPlugin extends cds.build.Plugin {
-  
-  static taskDefaults = { src: cds.env.folders.db }
-  
-  static hasTask() { return cds.requires.db?.kind === 'postgres' }
+cds.build?.register?.(
+  'postgres',
+  class PostgresBuildPlugin extends cds.build.Plugin {
+    static taskDefaults = { src: cds.env.folders.db }
 
-  init() {
-    // different from the default build output structure
-    this.task.dest = path.join(cds.root, cds.env.build.target !== '.' ? cds.env.build.target : 'gen', 'pg')
-  }
-
-  async build() {
-    const model = await this.model()
-    if (!model) return
-
-    const promises = []
-    if (fs.existsSync(path.join(this.task.src, 'package.json'))) {
-      promises.push(this.copy(path.join(this.task.src, 'package.json')).to('package.json'))
-    } else {
-      promises.push(
-        this.write({
-          dependencies: { '@sap/cds': '^7', '@cap-js/postgres': '^1' },
-          scripts: { start: 'cds-deploy' },
-        }).to('package.json'),
-      )
+    static hasTask () {
+      return cds.requires.db?.kind === 'postgres'
     }
-    promises.push(this.write(cds.compile.to.json(model)).to(path.join('db', 'csn.json')))
 
-    let data
-    if (fs.existsSync(path.join(this.task.src, 'data'))) {
-      data = 'data'
-    } else if (fs.existsSync(path.join(this.task.src, 'csv'))) {
-      data = 'csv'
+    init () {
+      // different from the default build output structure
+      this.task.dest = path.join(cds.root, cds.env.build.target !== '.' ? cds.env.build.target : 'gen', 'pg')
     }
-    if (data) {
-      promises.push(this.copy(data).to(path.join('db', 'data')))
+
+    async build () {
+      const model = await this.model()
+      if (!model) return
+
+      const promises = []
+      if (fs.existsSync(path.join(this.task.src, 'package.json'))) {
+        promises.push(this.copy(path.join(this.task.src, 'package.json')).to('package.json'))
+      } else {
+        promises.push(
+          this.write({
+            dependencies: { '@sap/cds': '^7', '@quadrio/postgres': '^1' },
+            scripts: { start: 'cds-deploy' },
+          }).to('package.json'),
+        )
+      }
+      promises.push(this.write(cds.compile.to.json(model)).to(path.join('db', 'csn.json')))
+
+      let data
+      if (fs.existsSync(path.join(this.task.src, 'data'))) {
+        data = 'data'
+      } else if (fs.existsSync(path.join(this.task.src, 'csv'))) {
+        data = 'csv'
+      }
+      if (data) {
+        promises.push(this.copy(data).to(path.join('db', 'data')))
+      }
+      return Promise.all(promises)
     }
-    return Promise.all(promises)
-  }
-})
+  },
+)
--- test/deploy.js
+++ test/deploy.js
@@ -1,3 +1,3 @@
-var { deploy } = require('@cap-js/postgres')
+var { deploy } = require('@quadrio/postgres')
 // eslint-disable-next-line no-console
 deploy('*').to('db').catch(console.error)
