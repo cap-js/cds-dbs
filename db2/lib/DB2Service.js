7a3a9dc26e73b4d33725d69bc727b54f228aefbe
Thanh-NgHoL <luffie.thanh@gmail.com>
2024-07-16 13:35:55 +0200

    tm[

--- db2/lib/DB2Service.js
+++ db2/lib/DB2Service.js
@@ -3,6 +3,7 @@ const ibmdb = require('ibm_db')
 const cds = require('@sap/cds')
 const crypto = require('crypto')
 const { Writeable, Readable } = require('stream')
+const DEBUG = cds.debug('sql|db')
 /**
  *
  */
@@ -37,7 +38,10 @@ class DB2Service extends SQLService {
           database: cds.env.requires.db.credentials.database,
         }
         const connStr = `DATABASE=${credentials.database};HOSTNAME=${credentials.host};UID=${credentials.user};PWD=${credentials.password};PORT=${credentials.port};PROTOCOL=TCPIP`
-        const dbc = await ibmdb.open(connStr)
+        // const dbc = await ibmdb.open(connStr)
+        const dbc = await ibmdb.open(
+          `DATABASE=testdb;HOSTNAME=localhost;UID=db2inst1;PWD=HariboMachtKinderFroh;PORT=50000;PROTOCOL=TCPIP`,
+        )
         return dbc
       },
       destroy: dbc => dbc.close(),
@@ -59,49 +63,79 @@ class DB2Service extends SQLService {
     return super.release()
   }
   prepare (sql) {
-    try {
-      const stmt = this.dbc
-      debugger
-    } catch (e) {
-      console.log(e)
-      throw e
+    // REVISIT: Make more efficient with Fetch or other functionalities from ibm_db
+    // const stmt = this.dbc.prepare(sql)
+    return {
+      run: async () => {
+        try {
+          const result = await this.dbc.query(sql)
+          return result
+        } catch (error) {
+          throw `${error}${sql}`
+        }
+      },
+      get: async () => {
+        try {
+          const result = await this.dbc.query(sql)
+          return result[0]
+        } catch (error) {
+          throw `${error}${sql}`
+        }
+      },
+      all: async () => {
+        try {
+          return await this.dbc.query(sql)
+        } catch (error) {
+          throw `${error}${sql}`
+        }
+      },
+      stream: async () => {
+        // TODO
+        try {
+          return this.dbc.queryStream(sql)
+        } catch (error) {
+          throw `${error}${sql}`
+        }
+      },
     }
   }
-
-  static CQN2SQL = class CQN2DB2 extends SQLService.CQN2SQL {
-    static Functions = require('./cql-functions')
-    static ReservedWords = { ...super.ReservedWords, ...require('./ReservedWords.json') }
-    //TODO: Types like UUID: () => `VARCHAR(36)`,
-    static TypeMap = {
-      ...super.TypeMap,
-    }
-    //TODO: InputConverters
-    static InputConverters = {
-      ...super.InputConverters,
-    }
-    static OutputConverters = {
-      ...super.OutputConverters,
-    }
+  exec (sql) {
+    return this.dbc.query(sql)
   }
-  async database ({ database }) {
-    // TODO get correct creds
+  // static CQN2SQL = class CQN2DB2 extends SQLService.CQN2SQL {
+  //   static Functions = require('./cql-functions')
+  //   static ReservedWords = { ...super.ReservedWords, ...require('./ReservedWords.json') }
+  //   //TODO: Types like UUID: () => `VARCHAR(36)`,
+  //   static TypeMap = {
+  //     ...super.TypeMap,
+  //   }
+  //   //TODO: InputConverters
+  //   static InputConverters = {
+  //     ...super.InputConverters,
+  //   }
+  //   static OutputConverters = {
+  //     ...super.OutputConverters,
+  //   }
+  // }
+  // async database ({ database }) {
+  //   // TODO get correct creds
 
-    try {
-      const con = await this.factory.create()
-      this.dbc = con
-    } finally {
-      if (this.dbc) {
-        await this.dbc.close()
-        delete this.dbc
+  //   try {
+  //     const con = await this.factory.create()
+  //     this.dbc = con
+  //   } finally {
+  //     if (this.dbc) {
+  //       await this.dbc.close()
+  //       delete this.dbc
 
-        await this.disconnect()
-      }
-    }
-  }
+  //       await this.disconnect()
+  //     }
+  //   }
+  // }
 
-  async tenant ({ database, tenant }, clean = false) {
-    //TODO
-  }
+  // async tenant ({ database, tenant }, clean = false) {
+  //   //TODO
+  // }
 }
 
 module.exports = DB2Service
