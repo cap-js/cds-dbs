99bcef241f828f5c94f78e8ab320b573f35ef13f
Dr. David A. Kunz <david.kunz@sap.com>
2024-09-02 11:52:09 +0200

    .

--- db-service/lib/deep-queries.js
+++ db-service/lib/deep-queries.js
@@ -2,7 +2,7 @@ const cds = require('@sap/cds')
 const { _target_name4 } = require('./SQLService')
 const InsertResult = require('../lib/InsertResults')
 
-const ROOT = Symbol('root') // also needed in tests
+const ROOT = Symbol('root')
 
 // REVISIT: remove old path with cds^8
 let _compareJson
