01de95f5050a1d3325459ccb78a4e9a1e0dbcfde
Dr. David A. Kunz <david.kunz@sap.com>
2024-09-03 13:42:11 +0200

    fix(deep): prevent false unique constraint errors and combine delete queries (#781)
    ```cds
    entity Foo : cuid {
      name: String @asser.unique;
    }
    ```
    
    
    existing: Foo( ID: 1, name: "foo" )
    
    DELETE: Foo( ID: 3, name: "bar" ) <-- can never fail
    UPDATE: Foo( ID: 1, name: "bar" ) <-- can prevent fail
    INSERT: Foo( ID: 2, name: "foo" )
    
    possible test:
    ```cds
    @odata.draft.enabled
    Books {
    key ID: Integer
    title: localized String;
    }
    ```
    
    ```js
    UPDATE(Books, 42).data({
    texts: [
    {locale: 'en', title: 'abc'}
    {locale: 'de', title: 'abc'}
    ]
    })
    ```
    
    ---------
    
    Co-authored-by: Bob den Os <108393871+BobdenOs@users.noreply.github.com>

--- db-service/test/deep/deep.test.js
+++ db-service/test/deep/deep.test.js
@@ -784,20 +784,28 @@ describe('test deep query generation', () => {
           ],
         },
       ])
-      const deepQueries = getDeepQueries(query, [], model.definitions.Root)
+      const { inserts, updates, deletes } = getDeepQueries(query, [], model.definitions.Root)
 
       const expectedInserts = [
         INSERT.into(model.definitions.Root)
           .entries([{ ID: 1 }, { ID: 2 }, { ID: 3 }]),
         INSERT.into(model.definitions.Child)
-          .entries([{ ID: 1 }, { ID: 2 }, { ID: 3 }, { ID: 4 }, { ID: 5 }, { ID: 6 }, { ID: 7 }, { ID: 8 }, { ID: 9 }]),
+          .entries([{ ID: 1 }, { ID: 2 }, { ID: 3 }, { ID: 4 }, { ID: 6 }, { ID: 7 }, { ID: 5 }, { ID: 9 }, { ID: 8 }]),
         INSERT.into(model.definitions.SubChild)
           .entries([{ ID: 10 }, { ID: 11 }, { ID: 12 }, { ID: 13 }]),
       ]
 
+      const insertsArray = Array.from(inserts.values())
+      const updatesArray = Array.from(updates)
+      const deletesArray = Array.from(deletes.values())
+
       expectedInserts.forEach(insert => {
-        expect(deepQueries).toContainEqual(insert)
+        expect(insertsArray).toContainEqual(insert)
       })
+
+      expect(updatesArray.length).toBe(0)
+      expect(deletesArray.length).toBe(0)
+
     })
 
     test('backlink keys are properly propagated', async () => {
