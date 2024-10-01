7faa0b2e9bdb0c6e35e142259becebb81752b521
Bob den Os <bob.den.os@sap.com>
2024-10-01 09:27:56 +0200

    fix timestamp $now test

--- test/compliance/timestamps.test.js
+++ test/compliance/timestamps.test.js
@@ -4,22 +4,21 @@ describe('datetime handling', () => {
   const { expect } = cds.test(__dirname + '/resources')
   test('datetime elements as key', async () => {
     let res
-    const payload = { dt: '2020-12-31T01:02:03Z', int: 4711}
-    res = await cds.db.run(INSERT.into('DateTimeEntity').entries(payload))
-    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(payload.dt).toISOString() }))
+    const payload = { dt: '2020-12-31T01:02:03Z', int: 4711 }
+    res = await INSERT.into('DateTimeEntity').entries(payload)
+    res = await SELECT.one.from('DateTimeEntity').where({ dt: new Date(payload.dt).toISOString() })
     expect(res).to.containSubset(payload)
-    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: new Date(payload.dt) }))
+    res = await SELECT.one.from('DateTimeEntity').where({ dt: new Date(payload.dt) })
     expect(res).to.containSubset(payload)
-    res = await cds.db.run(SELECT.one.from('DateTimeEntity').where({dt: payload.dt }))
+    res = await SELECT.one.from('DateTimeEntity').where({ dt: payload.dt })
     expect(res).to.containSubset(payload)
   })
 
-  test('$now in view', async () => {
+  test('$now in view', async () => cds.tx(async tx => {
     const req_timestamp = new Date()
-    await cds.db.run(INSERT.into('TimestampEntity').entries({ ID: 1, ts: req_timestamp }))
-    const { now } = await cds.db.run(SELECT.one.from('TimestampView'))
+    await INSERT.into('TimestampEntity').entries({ ID: 1, ts: req_timestamp })
+    const { now } = await SELECT.one.from('TimestampView')
     expect(now.match(/\.(\d\d\d)Z/)[1].match(/000/)).to.be.null //> check that we get ms precision
-    const diff = Math.abs(req_timestamp - Date.now())
-    expect(diff).to.be.lt(1000) //> check that we get the same timezone offset
-  })
+    expect(new Date(now) | 0).to.be.eq(tx.context.timestamp | 0)
+  }))
 })
