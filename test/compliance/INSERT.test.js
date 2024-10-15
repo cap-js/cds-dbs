3a928f757ed4719c2ceba75d2af104e067e2f152
Bob den Os <bob.den.os@sap.com>
2024-10-15 08:57:13 +0200

    Add efficient API INSERT tests

--- test/compliance/INSERT.test.js
+++ test/compliance/INSERT.test.js
@@ -1,6 +1,12 @@
-require('../cds')
+const cds = require('../cds')
+
+const { Readable } = require('stream')
 
 describe('INSERT', () => {
+  const { data, expect } = cds.test(__dirname + '/resources')
+  data.autoIsolation(true)
+  data.autoReset()
+
   describe('into', () => {
     test.skip('missing', () => {
       throw new Error('not supported')
@@ -8,8 +14,57 @@ describe('INSERT', () => {
   })
 
   describe('entries', () => {
-    test.skip('missing', () => {
-      throw new Error('not supported')
+    const genCount = 100
+    const gen = function* () {
+      for (let i = 0; i < genCount; i++)
+        yield { uuid: cds.utils.uuid() }
+    }
+
+    test('array', async () => {
+      const { uuid } = cds.entities('basic.literals')
+
+      await INSERT([...gen()]).into(uuid)
+
+      const result = await SELECT.from(uuid)
+      expect(result.length).to.eq(genCount)
+    })
+
+    test('iterator', async () => {
+      const { uuid } = cds.entities('basic.literals')
+
+      await INSERT(gen()).into(uuid)
+
+      const result = await SELECT.from(uuid)
+      expect(result.length).to.eq(genCount)
+    })
+
+    test('Readable (Object Mode)', async () => {
+      const { uuid } = cds.entities('basic.literals')
+
+      await INSERT(Readable.from(gen())).into(uuid)
+
+      const result = await SELECT.from(uuid)
+      expect(result.length).to.eq(genCount)
+    })
+
+    test('Readable (Raw Mode)', async () => {
+      const { uuid } = cds.entities('basic.literals')
+
+      const raw = function* (src) {
+        yield '['
+        let sep = ''
+        for (const obj of src) {
+          yield sep
+          yield JSON.stringify(obj)
+          sep = ','
+        }
+        yield ']'
+      }
+
+      await INSERT(Readable.from(raw(gen()), { objectMode: false })).into(uuid)
+
+      const result = await SELECT.from(uuid)
+      expect(result.length).to.eq(genCount)
     })
   })
 
