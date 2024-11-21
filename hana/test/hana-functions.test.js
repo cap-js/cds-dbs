0f5f2915d2034b7e238531d6ebdfed5110b154f2
Johannes Vogel <johannes.vogel@sap.com>
2024-11-21 14:32:52 +0100

    adapt test

--- hana/test/hana-functions.test.js
+++ hana/test/hana-functions.test.js
@@ -8,18 +8,18 @@ describe('HANA custom functions', () => {
       one: true,
       from: {ref: ['DUMMY']}, 
       columns: [
-        {func: 'current_utctimestamp', as: 'no_args'},
-        {func: 'current_utctimestamp', args: [{val: 0}], as: 'prec0'},
-        {func: 'current_utctimestamp', args: [{val: 3}], as: 'prec3'},
-        {func: 'current_utctimestamp', args: [{val: 7}], as: 'prec7'}] 
+        {func: 'current_utctimestamp', as: 'NO'},
+        {func: 'current_utctimestamp', args: [{val: 0}], as: 'P0'},
+        {func: 'current_utctimestamp', args: [{val: 3}], as: 'P3'},
+        {func: 'current_utctimestamp', args: [{val: 7}], as: 'P7'}] 
     }}
 
     const res = await cds.run(cqn)
 
-    expect(res.no_args.match(/\.(\d\d\d)0000/)).not.to.be.null // default 3
-    expect(res.prec0.match(/\.0000000/)).not.to.be.null
-    expect(res.prec3.match(/\.(\d\d\d)0000/)).not.to.be.null
-    expect(res.prec7.match(/\.(\d\d\d\d\d\d\d)/)).not.to.be.null
+    expect(res.NO.match(/\.(\d\d\d)0000/)).not.to.be.null // default 3
+    expect(res.P0.match(/\.0000000/)).not.to.be.null
+    expect(res.P3.match(/\.(\d\d\d)0000/)).not.to.be.null
+    expect(res.P7.match(/\.(\d\d\d\d\d\d\d)/)).not.to.be.null
 
 
   })
