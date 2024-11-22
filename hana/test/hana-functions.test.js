dd9373c6d2fc084b98a798ba90e393cc069dbc63
Johannes Vogel <johannes.vogel@sap.com>
2024-11-22 10:44:31 +0100

    reorder tests

--- hana/test/hana-functions.test.js
+++ hana/test/hana-functions.test.js
@@ -1,26 +1,54 @@
 const cds = require('../../test/cds')
 
-describe('HANA custom functions', () => {
+describe('HANA native functions', () => {
   const { expect } = cds.test(__dirname, 'fuzzy.cds')
 
-  test('current_utctimestamp', async () => {
-    const cqn = { SELECT: {
-      one: true,
-      from: {ref: ['DUMMY']}, 
-      columns: [
-        {func: 'current_utctimestamp', as: 'NO'},
-        {func: 'current_utctimestamp', args: [{val: 0}], as: 'P0'},
-        {func: 'current_utctimestamp', args: [{val: 3}], as: 'P3'},
-        {func: 'current_utctimestamp', args: [{val: 7}], as: 'P7'}] 
-    }}
+  describe('current_timestamp', () => {
+    test('no arguments', async () => {
+      const cqn = { SELECT: {
+        one: true,
+        from: {ref: ['DUMMY']}, 
+        columns: [
+          {func: 'current_utctimestamp', as: 'NO'},
+          {func: 'current_utctimestamp', args: [{val: 0}], as: 'P0'},
+          {func: 'current_utctimestamp', args: [{val: 3}], as: 'P3'},
+          {func: 'current_utctimestamp', args: [{val: 7}], as: 'P7'}] 
+      }}
+  
+      const res = await cds.run(cqn)
+  
+      expect(res.NO.match(/\.(\d\d\d)0000/)).not.to.be.null // default 3
+    })
 
-    const res = await cds.run(cqn)
+    test('0 skips ms precision', async () => {
+      const cqn = { SELECT: {
+        one: true,
+        from: {ref: ['DUMMY']}, 
+        columns: [
+          {func: 'current_utctimestamp', as: 'NO'},
+          {func: 'current_utctimestamp', args: [{val: 0}], as: 'P0'},
+          {func: 'current_utctimestamp', args: [{val: 3}], as: 'P3'},
+          {func: 'current_utctimestamp', args: [{val: 7}], as: 'P7'}] 
+      }}
+  
+      const res = await cds.run(cqn)
 
-    expect(res.NO.match(/\.(\d\d\d)0000/)).not.to.be.null // default 3
-    expect(res.P0.match(/\.0000000/)).not.to.be.null
-    expect(res.P3.match(/\.(\d\d\d)0000/)).not.to.be.null
-    expect(res.P7.match(/\.(\d\d\d\d\d\d\d)/)).not.to.be.null
+      expect(res.P0.match(/\.0000000/)).not.to.be.null
+    })
 
-
+    test('arbitrary values', async () => {
+      const cqn = { SELECT: {
+        one: true,
+        from: {ref: ['DUMMY']}, 
+        columns: [
+          {func: 'current_utctimestamp', args: [{val: 3}], as: 'P3'},
+          {func: 'current_utctimestamp', args: [{val: 7}], as: 'P7'}] 
+      }}
+  
+      const res = await cds.run(cqn)
+  
+      expect(res.P3.match(/\.(\d\d\d)0000/)).not.to.be.null
+      expect(res.P7.match(/\.(\d\d\d\d\d\d\d)/)).not.to.be.null
+    })
   })
 })
