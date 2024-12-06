38d911dab2c8018c75780484360379e0cbea6bbc
Bob den Os <bob.den.os@sap.com>
2024-12-06 14:08:01 +0100

    update abap test service definition

--- abap/test/service.js
+++ abap/test/service.js
@@ -3,23 +3,20 @@ const path = require('path')
 module.exports = {
   "impl": "@cap-js/abap",
   "credentials": {
-    "schema": "/ITAPC1/SQL_FLIGHTS_1",
+    "schema": process.env.ABAP_SCHEMA || 'SYS',
     "connectionString": [
-      // src: https://community.sap.com/t5/technology-blogs-by-sap/using-the-odbc-driver-for-abap-on-linux/ba-p/13513705
       `driver=${path.resolve(__dirname, '../bin/ODBC_driver_for_ABAP.so')}`,
-      'client=100', // TODO: see what this means
-      'trustall=true', // supersecure
+      'client=100',
+      'trustall=true',
       `CryptoLibrary=${path.resolve(__dirname, '../bin/libsapcrypto.so')}`,
 
-      // src: https://github.tools.sap/cap/dev/issues/1163
-      'host=25638c75-a54b-4658-8b04-3a1156f2c4f5.abap.eu10.hana.ondemand.com',
-      'port=443',
-      'servicePath=/sap/bc/sql/sql1/sap/s_privileged',
-      'Uid=ITAPC1_SQL_CAP_TESTS',
-      'Pwd=du2wPBiDdYbRbvvyGliPedKLvNHuWX_hzAvVNEH',
+      `host=${process.env.ABAP_HOST || 'localhost'}`,
+      `port=${process.env.ABAP_PORT || '443'}`,
+      `servicePath=${process.env.ABAP_PATH || '/sap/bc/sql/sql1/sap/s_privileged'}`,
+      `uid=${process.env.ABAP_USER || 'SYSTEM'}`,
+      `pwd=${process.env.ABAP_PASSWORD || 'Manager1'}`,
 
-      // src: https://github.wdf.sap.corp/orca/data_mart_management_service/blob/bf1ca50de56079cf3959f7f849f6a314b1cf7542/service/connections/models/SAPS4HanaCloud.ts#L668
-      'language=EN',
+      `language=${process.env.ABAP_LANG || 'EN'}`,
       'uidType=alias',
       'typeMap=semantic', // or native
     ].join(';')
