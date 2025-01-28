418e7f09601239a43d1c295b1852bae747d55747
Steffen Waldmann <steffen.waldmann@sap.com>
2025-01-28 19:59:04 +0100

    Reset more values

--- hana/lib/HANAService.js
+++ hana/lib/HANAService.js
@@ -50,7 +50,7 @@ class HANAService extends SQLService {
       max: 10,
       acquireTimeoutMillis,
       idleTimeoutMillis: 60000,
-      evictionRunIntervalMillis: 15000,
+      evictionRunIntervalMillis: 100000,
       numTestsPerEvictionRun: Math.ceil((this.options.pool?.max || 10) - (this.options.pool?.min || 0) / 3),
       ...(this.options.pool || {}),
       testOnBorrow: true,
