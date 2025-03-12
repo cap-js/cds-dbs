cap-js/cds-dbs

commit 163480b245b18a2829cd871c2f053c82bcc1abef

feat(forUpdate): ignore locked (#1074)
Unofficial, needed for outbox/task queue

hana/lib/HANAService.js
@@ -157,6 +157,14 @@ class HANAService extends SQLService {
       const resultQuery = query.clone()
       resultQuery.SELECT.forUpdate = undefined
       resultQuery.SELECT.forShareLock = undefined
+      const keys = Object.keys(req.target.keys || {})
+      if (keys.length && query.SELECT.forUpdate.ignoreLocked) {
+        // REVISIT: No support for count
+        // where [keys] in [values]   
+        const left = { list: keys.map(k => ({ ref: [k] })) }
+        const right = { list: rows.map(r => ({ list: keys.map(k => ({ val: r[k.toUpperCase()] })) })) }
+        resultQuery.SELECT.where = [left, 'in', right]
+      }
       return this.onSELECT({ query: resultQuery, __proto__: req })
     }
 

db-service/lib/cqn2sql.js
@@ -449,9 +449,10 @@ class CQN2SQLRenderer {
    * @returns {string} SQL
    */
   forUpdate(update) {
-    const { wait, of } = update
+    const { wait, of, ignoreLocked } = update
     let sql = 'FOR UPDATE'
     if (!_empty(of)) sql += ` OF ${of.map(x => this.expr(x)).join(', ')}`
+    if (ignoreLocked) sql += ' IGNORE LOCKED'
     if (typeof wait === 'number') sql += ` WAIT ${wait}`
     return sql
   }

postgres/lib/PostgresService.js
@@ -466,9 +466,11 @@ GROUP BY k
     // Postgres does not support locking columns only tables which makes of unapplicable
     // Postgres does not support "wait n" it only supports "nowait"
     forUpdate(update) {
-      const { wait } = update
-      if (wait === 0) return 'FOR UPDATE NOWAIT'
-      return 'FOR UPDATE'
+      const { wait, ignoreLocked } = update
+      let sql = 'FOR UPDATE'
+      if (wait === 0) sql += ' NOWAIT'
+      if (ignoreLocked) sql += ' SKIP LOCKED'
+      return sql
     }
 
     forShareLock(lock) {

test/compliance/SELECT.test.js
@@ -752,7 +752,7 @@ describe('SELECT', () => {
     })
   })
 
-  const generalLockTest = (lock4, shared = false) => {
+  const generalLockTest = (lock4, { shared = false, ignoreLocked = false } = {}) => {
     const isSQLite = () => cds.db.options.impl === '@cap-js/sqlite'
 
     const setMax = max => {
@@ -851,7 +851,7 @@ describe('SELECT', () => {
           await tx1.run(lock4(false))
 
           // Lock false
-          if (shared) {
+          if (shared || ignoreLocked) {
             const ret = await tx2.run(lock4(false))
             expect(ret).is.not.undefined
           } else {
@@ -885,7 +885,20 @@ describe('SELECT', () => {
 
     generalLockTest(bool => boolLock.clone()
       .where([{ ref: ['bool'] }, '=', { val: bool }]),
-      true
+      { shared: true }
+    )
+  })
+
+  describe('forUpdate ignore locked', () => {
+    const boolLock = SELECT.from('basic.projection.globals')
+      .forShareLock({
+        of: ['bool'],
+        ignoreLocked: true
+      })
+
+    generalLockTest(bool => boolLock.clone()
+      .where([{ ref: ['bool'] }, '=', { val: bool }]),
+      { ignoreLocked: true }
     )
   })
 

commit 1175b6e52a70814c1967387ae80e628944f5b71f

fix(forUpdate): forUpdate optional (#1079)
Error introduced in https://github.com/cap-js/cds-dbs/pull/1074

hana/lib/HANAService.js
@@ -158,11 +158,12 @@ class HANAService extends SQLService {
       resultQuery.SELECT.forUpdate = undefined
       resultQuery.SELECT.forShareLock = undefined
       const keys = Object.keys(req.target.keys || {})
-      if (keys.length && query.SELECT.forUpdate.ignoreLocked) {
+      if (keys.length && query.SELECT.forUpdate?.ignoreLocked) {
         // REVISIT: No support for count
         // where [keys] in [values]   
         const left = { list: keys.map(k => ({ ref: [k] })) }
         const right = { list: rows.map(r => ({ list: keys.map(k => ({ val: r[k.toUpperCase()] })) })) }
+        resultQuery.SELECT.limit = undefined
         resultQuery.SELECT.where = [left, 'in', right]
       }
       return this.onSELECT({ query: resultQuery, __proto__: req })