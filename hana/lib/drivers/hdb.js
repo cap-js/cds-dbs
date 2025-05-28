d2f3393a78f10f6245b8a7d43a8d7c8ffa76a016
Bob den Os <bob.den.os@sap.com>
2025-05-28 08:33:54 +0200

    always enable boolean support for hdb

--- hana/lib/drivers/hdb.js
+++ hana/lib/drivers/hdb.js
@@ -10,7 +10,7 @@ const { driver, prom, handleLevel } = require('./base')
 const { resultSetStream } = require('./stream')
 const { wrap_client } = require('./dynatrace')
 
-if (cds.env.features.sql_simple_queries === 3) {
+if (cds.env.features.sql_simple_queries) {
   // Make hdb return true / false
   const Reader = require('hdb/lib/protocol/Reader.js')
   Reader.prototype._readTinyInt = Reader.prototype.readTinyInt
