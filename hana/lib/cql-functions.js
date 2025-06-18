6ffdf60fa3daf6a675842f38d10d4f952f6a065b
I543501 <56645452+larsplessing@users.noreply.github.com>
2025-06-18 12:43:52 +0200

    Update cql-functions.js

--- hana/lib/cql-functions.js
+++ hana/lib/cql-functions.js
@@ -141,7 +141,7 @@ const StandardFunctions = {
               `Invalid configuration ${rank} for @Search.ranking. HIGH, MEDIUM, LOW are supported values.`,
             )
         }
-        fuzzy += ` MINIMAL TOKEN SCORE ${e.element?.['@Search.fuzzinessThreshold'] || fuzzyIndex} SIMILARITY CALCULATION MODE 'search'`
+        fuzzy += ` MINIMAL TOKEN SCORE ${e.element?.['@Search.fuzzinessThreshold'] || fuzzyIndex} SIMILARITY CALCULATION MODE 'search' search mode 'text'`
         // rewrite ref to xpr to mix in search config
         // ensure in place modification to reuse .toString method that ensures quoting
         e.xpr = [{ ref: e.ref }, fuzzy]
