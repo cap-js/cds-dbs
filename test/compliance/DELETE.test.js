a72cc3047b9cbbda0b67e0b5ca1adc4b9042c4ad
I543501 <56645452+larsplessing@users.noreply.github.com>
2025-07-10 10:49:57 +0200

    .

--- test/compliance/DELETE.test.js
+++ test/compliance/DELETE.test.js
@@ -4,6 +4,143 @@ const Child = 'complex.Child'
 const GrandChild = 'complex.GrandChild'
 const RootPWithKeys = 'complex.RootPWithKeys'
 const ChildPWithWhere = 'complex.ChildPWithWhere'
+const recusive = [
+  {
+    ID: 10,
+    fooRoot: 'Another Low Horror',
+    parent_ID: 5,
+    children: [
+      {
+        ID: 101,
+        fooChild: 'bar',
+        children: [
+          {
+            ID: 102,
+            fooGrandChild: 'bar',
+          },
+        ],
+      },
+      {
+        ID: 103,
+        fooChild: 'foo',
+        children: [
+          {
+            ID: 104,
+            fooGrandChild: 'foo',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    ID: 11,
+    fooRoot: 'Another Medium Horror',
+    parent_ID: 10,
+    children: [
+      {
+        ID: 111,
+        fooChild: 'bar',
+        children: [
+          {
+            ID: 112,
+            fooGrandChild: 'bar',
+          },
+        ],
+      },
+      {
+        ID: 113,
+        fooChild: 'foo',
+        children: [
+          {
+            ID: 114,
+            fooGrandChild: 'foo',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    ID: 12,
+    fooRoot: 'Another Hard Horror',
+    parent_ID: 11,
+    children: [
+      {
+        ID: 121,
+        fooChild: 'bar',
+        children: [
+          {
+            ID: 122,
+            fooGrandChild: 'bar',
+          },
+        ],
+      },
+      {
+        ID: 123,
+        fooChild: 'foo',
+        children: [
+          {
+            ID: 124,
+            fooGrandChild: 'foo',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    ID: 13,
+    fooRoot: 'Another Very Hard Horror',
+    parent_ID: 11,
+    children: [
+      {
+        ID: 131,
+        fooChild: 'bar',
+        children: [
+          {
+            ID: 132,
+            fooGrandChild: 'bar',
+          },
+        ],
+      },
+      {
+        ID: 133,
+        fooChild: 'foo',
+        children: [
+          {
+            ID: 134,
+            fooGrandChild: 'foo',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    ID: 14,
+    fooRoot: 'Another Very Very Hard Horror',
+    parent_ID: 12,
+    children: [
+      {
+        ID: 141,
+        fooChild: 'bar',
+        children: [
+          {
+            ID: 142,
+            fooGrandChild: 'bar',
+          },
+        ],
+      },
+      {
+        ID: 143,
+        fooChild: 'foo',
+        children: [
+          {
+            ID: 144,
+            fooGrandChild: 'foo',
+          },
+        ],
+      },
+    ],
+  },
+]
 
 describe('DELETE', () => {
   const { data, expect } = cds.test(__dirname + '/resources')
@@ -56,6 +193,25 @@ describe('DELETE', () => {
 
         const child = await cds.run(SELECT.from(Child).where({ ID: 6, or: { ID: 7 } }))
         expect(child.length).to.be.eq(0)
+
+        const grandchild = await cds.run(SELECT.from(GrandChild).where({ ID: 8, or: { ID: 9 } }))
+        expect(grandchild.length).to.be.eq(0)
+      })
+
+      test('on root with keys with recursive', async () => {
+        const insertsResp = await cds.run(INSERT.into(Root).entries(recusive))
+        expect(insertsResp.affectedRows).to.be.eq(5)
+        
+        const root2 = await cds.run(SELECT.from(Root))
+        const child2 = await cds.run(SELECT.from(Child))
+
+        const deepDelete = await cds.run(DELETE.from(RootPWithKeys).where({ ID: 5 }))
+        expect(deepDelete).to.be.eq(1)
+
+        const root = await cds.run(SELECT.from(Root))
+        expect(root.length).to.be.eq(0)
+        const child = await cds.run(SELECT.from(Child))
+        expect(child.length).to.be.eq(2)
 
         const grandchild = await cds.run(SELECT.from(GrandChild).where({ ID: 8, or: { ID: 9 } }))
         expect(grandchild.length).to.be.eq(0)
