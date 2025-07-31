cap-js/cds-dbs

commit 5a7b50cb02776cf6052c79bd276421dd87161882

fix: exists within expression is properly detected (#1156)
in a column expression, an `exists` predicate was not always properly
detected, the absence of this information lead to a an error because of
subsequent "dangling" filter conditions, which are usually not allowed
with the exception of being used in combination with the `exists`
predicate.

db-service/lib/infer/index.js
@@ -406,7 +406,10 @@ function infer(originalQuery, model) {
     if (arg.param || arg.SELECT) return // parameter references are only resolved into values on execution e.g. :val, :1 or ?
     if (arg.args) applyToFunctionArgs(arg.args, inferArg, [null, $baseLink, context])
     if (arg.list) arg.list.forEach(arg => inferArg(arg, null, $baseLink, context))
-    if (arg.xpr) arg.xpr.forEach(token => inferArg(token, queryElements, $baseLink, { ...context, inXpr: true })) // e.g. function in expression
+    if (arg.xpr)
+      arg.xpr.forEach((token, i) =>
+        inferArg(token, queryElements, $baseLink, { ...context, inXpr: true, inExists: arg.xpr[i - 1] === 'exists' }),
+      ) // e.g. function in expression
 
     if (!arg.ref) {
       if (arg.expand && queryElements) queryElements[arg.as] = resolveExpand(arg)

db-service/test/cqn4sql/where-exists.test.js
@@ -557,6 +557,28 @@ describe('EXISTS predicate in where', () => {
       }`)
     })
 
+    it('MUST handle negated where exists wrapped in xpr in CASE', () => {
+      let query = cqn4sql(
+        cds.ql`SELECT from bookshop.Books {
+        ID,
+        case when not (exists author[name = 'FOO'] or exists author[name = 'BAR']) then 'yes'
+             else 'no'
+        end as x
+       }`,
+        model,
+      )
+      expect(query).to.deep.equal(cds.ql`SELECT from bookshop.Books as $B {
+        $B.ID,
+        case when not (
+          exists (SELECT 1 from bookshop.Authors as $a where $a.ID = $B.author_ID and $a.name = 'FOO')
+          or
+          exists (SELECT 1 from bookshop.Authors as $a2 where $a2.ID = $B.author_ID and $a2.name = 'BAR')
+        ) then 'yes'
+             else 'no'
+        end as x
+      }`)
+    })
+
     it('MUST handle simple where exists with filter in CASE', () => {
       let query = cqn4sql(
         cds.ql`SELECT from bookshop.Books {

commit 37181a94e1c2d6f5837aa359291fee0e9226952e

fix: detect path expression inside nested xpr after `exists`
fix a regression introduced by https://github.com/cap-js/cds-dbs/pull/1156/files#diff-8392404ad436e8eeaa98fcbc648744a3ab8a4fea7fd94b2901b88700b0ccd53cR411

closes #1225

db-service/lib/infer/index.js
@@ -404,7 +404,7 @@ function infer(originalQuery, model) {
     if (arg.list) arg.list.forEach(arg => inferArg(arg, null, $baseLink, context))
     if (arg.xpr)
       arg.xpr.forEach((token, i) =>
-        inferArg(token, queryElements, $baseLink, { ...context, inXpr: true, inExists: arg.xpr[i - 1] === 'exists' }),
+        inferArg(token, queryElements, $baseLink, { ...context, inXpr: true, inExists: inExists || arg.xpr[i - 1] === 'exists' }),
       ) // e.g. function in expression
 
     if (!arg.ref) {

db-service/test/cqn4sql/where-exists.test.js
@@ -1604,6 +1604,17 @@ describe('path expression within infix filter following exists predicate', () =>
         )`,
     )
   })
+
+  it('path expression embedded in xpr', () => {
+    const q = cds.ql`select from bookshop.Books as Books { 1 as foo } where exists genre[('foo' || parent.name || 'bar') LIKE 'foo%bar']`
+    const expected = cds.ql`SELECT from bookshop.Books as Books { 1 as foo } where exists (
+      SELECT 1 from bookshop.Genres as $g
+      inner join bookshop.Genres as parent on parent.ID = $g.parent_ID
+      where $g.ID = Books.genre_ID and ('foo' || parent.name || 'bar') LIKE 'foo%bar'
+    )`
+    expect(cqn4sql(q, model)).to.deep.equal(expected)
+  })
+
   it('rejects the path expression at the leaf of scoped queries', () => {
     // original idea was to just add the `genre.name` as where clause to the query
     // however, with left outer joins we might get too many results