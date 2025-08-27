e2539295906d1b66b15adc8ea0edf4e7dd2258f3
Patrice Bender <patrice.bender@sap.com>
2025-08-27 14:57:43 +0200

    perf(`cqn4sql`): Add `cds-test` based benchmark suite
    based on https://github.com/cap-js/cds-test/pull/20 (and some local tweaks)
    
    preferred over #1329

---
+++ db-service/bench/cqn4sql/performance-benchmarks.test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const cds = require('@sap/cds')
+
+let cqn4sql = require('../../lib/cqn4sql')
+
+const {
+  perf,
+} = cds.test
+
+const report = (what, options) => perf.report(what, { store: true, stats: { requests: true }, ...options })
+
+describe('cqn4sql performance benchmarks', () => {
+  beforeEach(async () => {
+    const m = await cds.load([__dirname + '/model/schema']).then(cds.linked)
+    const orig = cqn4sql // keep reference to original to avoid recursion
+    cqn4sql = q => orig(q, m)
+  })
+
+  runBenchmarkFor('select simple', cds.ql`SELECT from my.Books { ID }`)
+
+  runBenchmarkFor('expand simple', cds.ql`SELECT from my.Authors { ID, books { title } }`)
+  runBenchmarkFor('expand recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent { parent { parent { name }}} }`)
+
+  runBenchmarkFor('exists simple', cds.ql`SELECT from my.Genres { ID, parent { parent { parent { name }}} }`)
+  runBenchmarkFor('exists recursive (depth 3)', cds.ql`SELECT from my.Genres { ID } where exists parent.parent.parent`)
+
+  runBenchmarkFor('assoc2join simple', cds.ql`SELECT from my.Books { ID, author.name }`)
+  runBenchmarkFor('assoc2join recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent.parent.parent.name }`)
+
+})
+
+
+function runBenchmarkFor(name, cqn) {
+  it(name, async () => report(await perf.fn(() => {
+    cqn4sql(cqn)
+  }), { title: name }))
+}
