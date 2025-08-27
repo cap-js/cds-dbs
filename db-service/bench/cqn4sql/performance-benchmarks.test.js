f31fbb689656d7d2b6da34505a95ff996675d969
Patrice Bender <patrice.bender@sap.com>
2025-08-27 18:51:47 +0200

    only run if requested

--- db-service/bench/cqn4sql/performance-benchmarks.test.js
+++ db-service/bench/cqn4sql/performance-benchmarks.test.js
@@ -1,44 +1,46 @@
 'use strict'
 
-const cds = require('@sap/cds')
-const { writeDump } = require('./utils/format-benchmarks')
-
-let cqn4sql = require('../../lib/cqn4sql')
-
-const {
-  perf,
-} = cds.test
-
-const report = (what, options) => perf.report(what, { store: true, stats: { requests: true }, ...options })
-
-describe('cqn4sql performance benchmarks', () => {
-  beforeEach(async () => {
-    const m = await cds.load([__dirname + '/model/schema']).then(cds.linked)
-    const orig = cqn4sql // keep reference to original to avoid recursion
-    cqn4sql = q => orig(q, m)
+if (process.env.CDS_BENCH === 'true') {
+  const cds = require('@sap/cds')
+  const { writeDump } = require('./utils/format-benchmarks')
+  
+  let cqn4sql = require('../../lib/cqn4sql')
+  
+  const {
+    perf,
+  } = cds.test
+  
+  const report = (what, options) => perf.report(what, { store: true, stats: { requests: true }, ...options })
+  
+  describe('cqn4sql performance benchmarks', () => {
+    beforeEach(async () => {
+      const m = await cds.load([__dirname + '/model/schema']).then(cds.linked)
+      const orig = cqn4sql // keep reference to original to avoid recursion
+      cqn4sql = q => orig(q, m)
+    })
+  
+    after('format & write dump', () => {
+      writeDump({ testFile: __filename })
+    })
+  
+    runBenchmarkFor('select simple', cds.ql`SELECT from my.Books { ID }`)
+  
+    runBenchmarkFor('expand simple', cds.ql`SELECT from my.Authors { ID, books { title } }`)
+    runBenchmarkFor('expand recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent { parent { parent { name }}} }`)
+  
+    runBenchmarkFor('exists simple', cds.ql`SELECT from my.Genres { ID } where exists parent`)
+    runBenchmarkFor('exists simple with path expression', cds.ql`SELECT from my.Genres { ID } where exists parent[parent.name = 'foo']`)
+    runBenchmarkFor('exists recursive (depth 3)', cds.ql`SELECT from my.Genres { ID } where exists parent.parent.parent`)
+  
+    runBenchmarkFor('assoc2join simple', cds.ql`SELECT from my.Books { ID, author.name }`)
+    runBenchmarkFor('assoc2join recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent.parent.parent.name }`)
+  
   })
-
-  after('format & write dump', () => {
-    writeDump({ testFile: __filename })
-  })
-
-  runBenchmarkFor('select simple', cds.ql`SELECT from my.Books { ID }`)
-
-  runBenchmarkFor('expand simple', cds.ql`SELECT from my.Authors { ID, books { title } }`)
-  runBenchmarkFor('expand recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent { parent { parent { name }}} }`)
-
-  runBenchmarkFor('exists simple', cds.ql`SELECT from my.Genres { ID } where exists parent`)
-  runBenchmarkFor('exists simple with path expression', cds.ql`SELECT from my.Genres { ID } where exists parent[parent.name = 'foo']`)
-  runBenchmarkFor('exists recursive (depth 3)', cds.ql`SELECT from my.Genres { ID } where exists parent.parent.parent`)
-
-  runBenchmarkFor('assoc2join simple', cds.ql`SELECT from my.Books { ID, author.name }`)
-  runBenchmarkFor('assoc2join recursive (depth 3)', cds.ql`SELECT from my.Genres { ID, parent.parent.parent.name }`)
-
-})
-
-
-function runBenchmarkFor(name, cqn) {
-  it(name, async () => report(await perf.fn(() => {
-    cqn4sql(cqn)
-  }, { title: name })))
+  
+  
+  function runBenchmarkFor(name, cqn) {
+    it(name, async () => report(await perf.fn(() => {
+      cqn4sql(cqn)
+    }, { title: name })))
+  }  
 }
