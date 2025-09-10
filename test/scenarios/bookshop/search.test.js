8a088882600d96ab82b6ead22ee034e7bed72985
Steffen Waldmann <steffen.waldmann@sap.com>
2025-09-10 13:02:39 +0200

    Fix search scenario tests

--- test/scenarios/bookshop/search.test.js
+++ test/scenarios/bookshop/search.test.js
@@ -8,4 +8,6 @@
  * Tests which make sure that the search queries are semantically correct can be found
  * in @file cds-dbs/test/compliance/SELECT.test.js
  */
-describe.skip('Bookshop - Search', () => {})
+describe.skip('Bookshop - Search', () => {
+  test.skip('placeholder', () => {})
+})
