569f7cf34dd5937c72cdbcb7b4ff76c0757dfbe0
I548646 <paul.erlenwein@sap.com>
2025-09-17 12:05:21 +0200

    test: insert from select

--- hana/test/column-order.test.js
+++ hana/test/column-order.test.js
@@ -4,23 +4,22 @@ const bookshop = cds.utils.path.resolve(__dirname, '../../test/bookshop')
 describe('column order', () => {
   const { expect } = cds.test(bookshop)
 
-  describe('regardless of column order specifed in query', () => {
-    let hanaService
+  let hanaService
+  beforeAll(async () => {
+    hanaService = await cds.connect.to('db')
+  })
 
-    beforeAll(async () => {
-      hanaService = await cds.connect.to('db')
-    })
+  const expectSqlScriptToBeEqual = (query1, query2) => {
+    const sql1 = hanaService.cqn2sql(query1)
+    const sql2 = hanaService.cqn2sql(query2)
+    expect(sql1.sql).to.equal(sql2.sql)
 
-    const expectSqlScriptToBeEqual = (query1, query2) => {
-      const sql1 = hanaService.cqn2sql(query1)
-      const sql2 = hanaService.cqn2sql(query2)
-      expect(sql1.sql).to.equal(sql2.sql)
+    const sqlScript1 = hanaService.wrapTemporary(sql1.temporary, sql1.withclause, sql1.blobs)
+    const sqlScript2 = hanaService.wrapTemporary(sql2.temporary, sql2.withclause, sql2.blobs)
+    expect(sqlScript1).to.equal(sqlScript2)
+  }
 
-      const sqlScript1 = hanaService.wrapTemporary(sql1.temporary, sql1.withclause, sql1.blobs)
-      const sqlScript2 = hanaService.wrapTemporary(sql2.temporary, sql2.withclause, sql2.blobs)
-      expect(sqlScript1).to.equal(sqlScript2)
-    }
-
+  describe('when selecting - regardless of column order specifed in query', () => {
     test('should select columns in the same order', async () => {
       const query1 = SELECT.from('sap.capire.bookshop.Books').columns(['ID', 'title', 'descr', 'stock', 'price'])
       const query2 = SELECT.from('sap.capire.bookshop.Books').columns(['stock', 'title', 'price', 'ID', 'descr'])
@@ -118,6 +117,35 @@ describe('column order', () => {
       const query2 = SELECT.from('sap.capire.bookshop.Books').columns([{ val: 1 }, 'ID'])
 
       expectSqlScriptToBeEqual(query1, query2)
+    })
+  })
+
+  describe('when inserting from select - regardless of column order specifed in query', () => {
+    test('should insert and select from columns in the same order', async () => {
+      const query1 = INSERT.into('sap.capire.bookshop.Books')
+        .columns(['ID', 'title', 'descr', 'stock', 'price'])
+        .from(SELECT.from('sap.capire.bookshop.Books').columns(['ID', 'title', 'descr', 'stock', 'price']))
+      const query2 = INSERT.into('sap.capire.bookshop.Books')
+        .columns(['stock', 'title', 'price', 'ID', 'descr'])
+        .from(SELECT.from('sap.capire.bookshop.Books').columns(['stock', 'title', 'price', 'ID', 'descr']))
+
+      expectSqlScriptToBeEqual(query1, query2)
+    })
+
+    test('should throw when selected columns are not specified', async () => {
+      const query1 = INSERT.into('sap.capire.bookshop.Books')
+        .columns(['ID', 'title', 'descr', 'stock', 'price'])
+        .from(SELECT.from('sap.capire.bookshop.Books'))
+      
+        expect(() => hanaService.cqn2sql(query1)).to.throw(/selected columns must be specified/i)
+    })
+
+    test('should throw when selected columns include *', async () => {
+      const query1 = INSERT.into('sap.capire.bookshop.Books')
+        .columns(['ID', 'title'])
+        .from(SELECT.from('sap.capire.bookshop.Books').columns(['ID', '*']))
+
+      expect(() => hanaService.cqn2sql(query1)).to.throw(/not inferred and includes '*'/i)
     })
   })
 })
