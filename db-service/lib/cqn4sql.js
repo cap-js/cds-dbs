e62537fd55e3555836933564a799ff36d2fae158
Patrice Bender <patrice.bender@sap.com>
2025-10-24 14:22:37 +0200

    consider case where there is no where

--- db-service/lib/cqn4sql.js
+++ db-service/lib/cqn4sql.js
@@ -2415,7 +2415,7 @@ function assignQueryModifiers(SELECT, modifiers) {
       else SELECT.having.push('and', ...val)
     } else if (key === 'where') {
       // ignore OData shortcut variant: `… bookshop.Orders:items[2]`
-      if(val.length === 1 && val[0].val) continue
+      if(!val || val.length === 1 && val[0].val) continue
       if (!SELECT.where) SELECT.where = val
       // infix filter comes first in resulting where
       else SELECT.where = [...(hasLogicalOr(val) ? [asXpr(val)] : val), 'and', ...(hasLogicalOr(SELECT.where) ? [asXpr(SELECT.where)] : SELECT.where)]
