10b4d2d9c90f6d1b852f720d9321a3da8e10b12a
D050513 <sebastian.van.syckel@sap.com>
2025-11-06 10:17:27 +0100

    chore: reduce require internals

--- test/sflight/test/odata.test.js
+++ test/sflight/test/odata.test.js
@@ -1,4 +1,4 @@
-const cds = require('@sap/cds/lib')
+const cds = require('@sap/cds')
 const { GET, POST, PATCH, axios, expect } = cds.test(__dirname+'/..')
 const EDIT = (url) => POST (url+'/TravelService.draftEdit',{})
 const SAVE = (url) => POST (url+'/TravelService.draftActivate')
