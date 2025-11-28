141d8d885f9e8bde8713ffa667517f03cb400d12
D051920 <v.kozyura@sap.com>
2025-11-28 12:35:16 +0100

    use cds.test expect

--- test/scenarios/bookshop/runtime-views.test.js
+++ test/scenarios/bookshop/runtime-views.test.js
@@ -1,10 +1,8 @@
 const cds = require('../../cds.js')
 const bookshop = cds.utils.path.resolve(__dirname, '../../bookshop')
 
-const { fail } = require('assert')
-
 describe('Runtime Views', () => {
-  cds.test(bookshop)
+  const { expect } = cds.test(bookshop)
 
   beforeAll(async () => {
     cds.env.features.runtime_views = true
@@ -18,19 +16,19 @@ describe('Runtime Views', () => {
     describe('Basic Book Queries', () => {
       test('runtimeViews0.Book with ID field should return correct book data', async () => {
         const res = await SELECT.from('runtimeViews0Service.Book').where({ ID: 201 })
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.include({
           ID: 201,
           title: 'Wuthering Heights',
         })
         const resDeployed = await SELECT.from('views0Service.Book').where({ ID: 201 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
 
       test('runtimeViews1.Book with id field and author should return correct book data', async () => {
         const res = await SELECT.from('runtimeViews1Service.Book').where({ id: 201 })
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.include({
           id: 201,
           title: 'Wuthering Heights',
           authorName: 'Emily Brontë',
@@ -38,7 +36,7 @@ describe('Runtime Views', () => {
           author_placeOfBirth: 'Thornton, Yorkshire',
         })
         const resDeployed = await SELECT.from('views1Service.Book').where({ id: 201 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
     })
 
@@ -46,26 +44,26 @@ describe('Runtime Views', () => {
       test('nested projection with basic fields', async () => {
         const res = await SELECT.from('runtimeViews2Service.Book').columns(['id']).where({ id: 201 })
 
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.include({
           id: 201,
         })
 
         const resDeployed = await SELECT.from('views2Service.Book').columns(['id']).where({ id: 201 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
 
       test('nested projection with expand', async () => {
         const res = await SELECT.from('runtimeViews2Service.Book').columns(['id', 'Authorid']).where({ id: 201 })
 
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.include({
           id: 201,
           Authorid: 101,
         })
 
         const resDeployed = await SELECT.from('views2Service.Book').columns(['id', 'Authorid']).where({ id: 201 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
     })
 
@@ -75,8 +73,8 @@ describe('Runtime Views', () => {
           .columns(['id', 'title', 'AuthorName'])
           .where({ id: 201 })
 
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.include({
           id: 201,
           title: 'Wuthering Heights',
           AuthorName: 'Emily Brontë',
@@ -85,7 +83,7 @@ describe('Runtime Views', () => {
         const resDeployed = await SELECT.from('views2Service.Book')
           .columns(['id', 'title', 'AuthorName'])
           .where({ id: 201 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
 
       test('select with alias, filter, and ordering', async () => {
@@ -94,13 +92,13 @@ describe('Runtime Views', () => {
           .where('id = 201 or id = 207')
           .orderBy('title')
 
-        expect(res).toHaveLength(2)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(2)
+        expect(res[0]).to.include({
           id: 207,
           title: 'Jane Eyre',
           AuthorName: 'Charlotte Brontë',
         })
-        expect(res[1]).toMatchObject({
+        expect(res[1]).to.include({
           id: 201,
           title: 'Wuthering Heights',
           AuthorName: 'Emily Brontë',
@@ -110,7 +108,7 @@ describe('Runtime Views', () => {
           .columns(['id', 'title', 'AuthorName'])
           .where('id = 201 or id = 207')
           .orderBy('title')
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
     })
 
@@ -122,13 +120,13 @@ describe('Runtime Views', () => {
           .orderBy('title')
 
         const authors = res.map(b => b.author.name)
-        expect(authors).toEqual(expect.arrayContaining(['Charlotte Brontë', 'Edgar Allen Poe', 'Richard Carpenter']))
+        expect(authors).to.deep.equal(['Richard Carpenter', 'Edgar Allen Poe', 'Charlotte Brontë', 'Edgar Allen Poe'])
 
         const resDeployed = await SELECT.from('views0Service.Book')
           .columns([{ ref: ['author'], expand: ['*'] }])
           .where('ID != 201')
           .orderBy('title')
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
 
       test('group by with aggregate count', async () => {
@@ -138,19 +136,20 @@ describe('Runtime Views', () => {
           .groupBy('AuthorName')
           .orderBy('AuthorName')
 
-        expect(res).toEqual(
-          expect.arrayContaining([
+        expect(res).to.deep.equal([
             { AuthorName: 'Charlotte Brontë', books: 1 },
+            { AuthorName: 'Edgar Allen Poe', books: 2},
             { AuthorName: 'Emily Brontë', books: 1 },
-          ]),
-        )
+            { AuthorName: 'Richard Carpenter', books: 1 }
+        ])
+        
 
         const resDeployed = await SELECT.from('views2Service.Book')
           .columns(['AuthorName', 'count(*) as books'])
           .where('AuthorName IS NOT NULL')
           .groupBy('AuthorName')
           .orderBy('AuthorName')
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
     })
 
@@ -158,15 +157,15 @@ describe('Runtime Views', () => {
       test('runtime with books redirected', async () => {
         const { AuthorRedirected: RTView } = cds.entities('runtimeViews0Service')
         const res = await cds.ql`select from ${RTView} { ID, books { title } } where ID = ${101}`
-        expect(res).toHaveLength(1)
-        expect(res[0]).toMatchObject({
+        expect(res).to.have.length(1)
+        expect(res[0]).to.deep.include({
           ID: 101,
           books: [
             { title: 'Redirected Wuthering Heights' }
           ]
         })
         const resDeployed = await SELECT.from('views0Service.AuthorRedirected').columns(['ID', { expand: [{ref: ['title']}], ref: ['books'] }]).where({ ID: 101 })
-        expect(res).toEqual(resDeployed)
+        expect(res).to.deep.equal(resDeployed)
       })
     })
   })
@@ -175,46 +174,25 @@ describe('Runtime Views', () => {
     describe('Unsupported Entity Types', () => {
       test('Virtual entities should throw error', async () => {
         const { VirtualBookView } = cds.entities('runtimeViewsErrorService')
-        try { await cds.ql`select from ${VirtualBookView}` }
-        catch (error) { expect(error.message).toMatch(/is not a runtime view/) }
+        await expect(cds.ql`select from ${VirtualBookView}`).to.be.rejectedWith(/is not a runtime view/)
       })      
     })
 
     describe('Field Access Restrictions', () => {
       test('excluded fields should not be accessible', async () => {
-        try {
-          await SELECT.from('runtimeViews2Service.Book').columns(['stock'])
-          fail('Expected request to throw an error')
-        } catch (error) {
-          expect(error.message).toMatch(/"stock" not found/)
-        }
+        await expect(SELECT.from('runtimeViews2Service.Book').columns(['stock'])).to.be.rejectedWith(/"stock" not found/)
       })
 
       test('excluded fields with alias should not be accessible', async () => {
-        try {
-          await SELECT.from('runtimeViews2Service.Book').columns(['stock'])
-          fail('Expected request to throw an error')
-        } catch (error) {
-          expect(error.message).toMatch(/"stock" not found/)
-        }
+        await expect(SELECT.from('runtimeViews2Service.Book').columns(['stock'])).to.be.rejectedWith(/"stock" not found/)
       })
 
       test('View with UNION should throw DB error', async () => {
-        try {
-          await SELECT.from('runtimeViews0Service.AuthorsAndBooks')
-          fail('Expected request to throw an error')
-        } catch (error) {
-          expect(error.message).toMatch(/no such table|invalid table name|does not exist/)
-        }
+        await expect(SELECT.from('runtimeViews0Service.AuthorsAndBooks')).to.be.rejectedWith(/no such table|invalid table name|does not exist/)
       })
 
       test('View with JOIN should throw DB error', async () => {
-        try {
-          await SELECT.from('runtimeViews0Service.BookWithEditions')
-          fail('Expected request to throw an error')
-        } catch (error) {
-          expect(error.message).toMatch(/no such table|invalid table name|does not exist/)
-        }
+        await expect(SELECT.from('runtimeViews0Service.BookWithEditions')).to.be.rejectedWith(/no such table|invalid table name|does not exist/)
       })
     })
   })
