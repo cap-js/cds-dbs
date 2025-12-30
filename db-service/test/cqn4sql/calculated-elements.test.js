eb6293d17ec00cbc6cb15b214dcda2252705ed2b
D045778 <johannes.vogel@sap.com>
2025-12-30 11:15:07 +0100

    add test for calc elements with subselects

--- db-service/test/cqn4sql/calculated-elements.test.js
+++ db-service/test/cqn4sql/calculated-elements.test.js
@@ -1086,4 +1086,14 @@ describe('Unfolding calculated elements and localized', () => {
     expected.SELECT.localized = true
     expect(query).to.deep.equal(expected)
   })
+
+  it('calc elements with sub selects', () => {
+    const q = cds.ql`SELECT ID, area from (SELECT ID, area from booksCalc.Books)`
+    let query = cqn4sql(q, model)
+        const expected = cds.ql`SELECT __select__.ID, __select__.area from ( SELECT FROM booksCalc.Books as $B {
+        $B.ID,
+        $B.length * $B.width as area
+  }) AS __select__`
+    expect(query).to.deep.equal(expected)
+  })
 })
