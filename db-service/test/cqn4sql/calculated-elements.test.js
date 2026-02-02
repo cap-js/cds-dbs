4e6bb6552d21ef4b9734eef1174d30a3de285476
D045778 <johannes.vogel@sap.com>
2026-02-02 14:02:38 +0100

    add test for intermediate select *

--- db-service/test/cqn4sql/calculated-elements.test.js
+++ db-service/test/cqn4sql/calculated-elements.test.js
@@ -1155,5 +1155,18 @@ describe('Unfolding calculated elements and localized', () => {
       ) AS __select__`
     expect(query).to.deep.equal(expected)
   })
+
+  it('calc elements with intermediate sub select star', () => {
+    const q = cds.ql`SELECT ID, area from (SELECT * FROM (SELECT ID, area from booksCalc.Books))`
+    let query = cqn4sql(q, model)
+    const expected = cds.ql`
+      SELECT __select__.ID, __select__.area from (
+          SELECT FROM booksCalc.Books as $B {
+            $B.ID,
+            $B.length * $B.width as area
+          }
+      ) AS __select__`
+    expect(query).to.deep.equal(expected)
+  })
 })
 
