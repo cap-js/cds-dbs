fa5a766b622e4b4c0be0643216e5ec9ea8a887b5
D045778 <johannes.vogel@sap.com>
2026-02-12 10:36:39 +0100

    fix: sqlite generated key is named lastInsertRowid

--- db-service/lib/InsertResults.js
+++ db-service/lib/InsertResults.js
@@ -71,7 +71,7 @@ module.exports = class InsertResult {
     // If no generated keys in entries/rows/values we might have database-generated keys
     const rows = this.results.slice(0, this.affectedRows) // only up to # of root entries
     return (super[iterator] = function* () {
-      for (const each of rows) yield { [k1]: this.insertedRowId4(each) } // REVISIT: sqlite only returns a single lastID per row -> how is that with others?
+      for (const each of rows) yield { [k1]: this.insertedRowId4(each) } // REVISIT: sqlite only returns a single lastInsertRowid per row -> how is that with others?
     })
   }
 
@@ -99,7 +99,7 @@ module.exports = class InsertResult {
    * @returns {number}
    */
   insertedRowId4(result) {
-    return result.lastID
+    return result.lastInsertRowid
   }
 
   /**
