65f78e1f3af83188462e9d44db67daa5d743ceb0
Patrice Bender <patrice.bender@sap.com>
2026-02-24 12:43:13 +0100

    fix: enable expressions for `inline` (#1512)
    this was not considered so far and _any_ expression within a (nested)
    inline led to a type error.
    
    With this change, the robustness of the `inline`
    directive has been improved and can handle all kinds of (path)
    expressions in the nested structure.

--- db-service/lib/infer/index.js
+++ db-service/lib/infer/index.js
@@ -725,7 +725,7 @@ function infer(originalQuery, model) {
         } else {
           const nameParts = namePrefix ? [namePrefix] : []
           if (inlineCol.as) nameParts.push(inlineCol.as)
-          else nameParts.push(...inlineCol.ref.map(idOnly))
+          else if (inlineCol.ref) nameParts.push(...inlineCol.ref.map(idOnly))
           const name = nameParts.join('_')
           if (inlineCol.inline) {
             const inlineElements = resolveInline(inlineCol, name)
@@ -736,6 +736,8 @@ function infer(originalQuery, model) {
           } else if (inlineCol.val) {
             elements[name] = getCdsTypeForVal(inlineCol.val)
           } else if (inlineCol.func) {
+            elements[name] = {}
+          } else if (inlineCol.xpr) {
             elements[name] = {}
           } else {
             elements[name] = inlineCol.$refLinks[inlineCol.$refLinks.length - 1].definition
