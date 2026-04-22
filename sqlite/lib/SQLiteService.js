b39c54cb85de483c7a0e6dd757a0df9b19b1f470
I548646 <paul.erlenwein@sap.com>
2026-04-22 10:53:09 +0200

    fix: switch back to cast

--- sqlite/lib/SQLiteService.js
+++ sqlite/lib/SQLiteService.js
@@ -274,7 +274,7 @@ class SQLiteService extends SQLService {
         ? (expr, elem) =>
             elem?.scale
               ? `CASE WHEN ${expr} IS NULL THEN NULL ELSE format('%.${elem.scale}f', ${expr}) END`
-              : `CASE WHEN ${expr} IS NULL THEN NULL ELSE format('%!f', ${expr}) END`
+              : `CAST(${expr} AS TEXT)`
         : undefined,
       // Binary is not allowed in json objects
       Binary: expr => `${expr} || ''`,
