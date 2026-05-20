6d29113e88b641e9ad7b1bb03476ffc8b0060aab
d049904 <heiko.witteborg@sap.com>
2026-05-20 13:45:58 +0200

    Revert "fix: await release() in commit and rollback"
    This reverts commit b30b923c415cd6cff4b1e370b5ea0551d7fc11b5.

--- db-service/lib/common/DatabaseService.js
+++ db-service/lib/common/DatabaseService.js
@@ -83,7 +83,7 @@ class DatabaseService extends cds.Service {
   async commit() {
     if (!this.dbc) return
     await this.send('COMMIT')
-    await this.release() // only release on successful commit as otherwise released on rollback
+    this.release() // only release on successful commit as otherwise released on rollback
   }
 
   /**
@@ -94,7 +94,7 @@ class DatabaseService extends cds.Service {
     try {
       await this.send('ROLLBACK')
     } finally {
-      await this.release()
+      this.release()
     }
   }
 
