ab8ac221fda0792b1cd0d12adf477a52eaf8ca90
I543501 <lars.plessing@sap.com>
2026-07-28 17:31:16 +0200

    potential fix for assocs

--- db-service/lib/cqn2sql.js
+++ db-service/lib/cqn2sql.js
@@ -919,7 +919,7 @@ class CQN2SQLRenderer {
       this.entries = [[...this.values, stream]]
     }
 
-    const extractions = this._managed = this.managed(columns.map(c => ({ name: c })), { ...transitions.target.elements, ...elements })
+    const extractions = this._managed = this.managed(columns.map(c => ({ name: c })), { ...transitions.target.elements, ...elements }).filter(e => !transitions.mapping.get(e.name)?.ref[1])
     return (this.sql = `INSERT INTO ${this.quote(entity)}${alias ? ' as ' + this.quote(alias) : ''} (${extractions.map(c => this.quote(transitions.mapping.get(c.name)?.ref?.[0] || c.name))
       }) SELECT ${extractions.map(c => c.insert)} FROM json_each(?)`)
   }
