512536ee90084615ad79188291a90a977c394315
I548646 <paul.erlenwein@sap.com>
2026-08-04 14:21:34 +0200

    test: add failing test for reference

---
+++ test/scenarios/bookshop/pql.test.js
@@ -0,0 +1,23 @@
+const cds = require('../../cds.js')
+const bookshop = cds.utils.path.resolve(__dirname, '../../bookshop')
+
+const admin = {
+  auth: {
+    username: 'alice',
+  },
+}
+
+describe('Bookshop - pql', () => {
+  cds.log('pql', 'debug')
+  const { GET, expect } = cds.test(bookshop)
+
+  test('groupby simple properties', async () => {
+    const res = await GET('/admin/Books?$apply=groupby((ID))', admin)
+    expect(res.status).to.be.eq(200)
+  })
+
+  test('groupby path expression', async () => {
+    const res = await GET('/admin/Books?$apply=groupby((author/placeOfBirth))', admin)
+    expect(res.status).to.be.eq(200)
+  })
+})
